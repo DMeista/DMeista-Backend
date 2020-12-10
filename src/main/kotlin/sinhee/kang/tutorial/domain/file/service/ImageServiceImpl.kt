fdc108a1a017fb9385397314ec4fa37dcfd8610b
kangsinhee <rkdtlsgml40@naver.com>
2020-12-10 23:42:16 +0900

    [CREATE]Image Service

--- src/main/kotlin/sinhee/kang/tutorial/domain/file/service/ImageServiceImpl.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/file/service/ImageServiceImpl.kt
@@ -3,16 +3,24 @@ package sinhee.kang.tutorial.domain.file.service
 import org.apache.commons.io.IOUtils
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import sinhee.kang.tutorial.domain.file.domain.ImageFile
+import sinhee.kang.tutorial.domain.file.domain.repository.ImageFileRepository
 import sinhee.kang.tutorial.domain.file.exception.ImageNotFoundException
+import sinhee.kang.tutorial.domain.post.domain.post.Post
 
 import java.io.File
 import java.io.FileInputStream
 import java.io.InputStream
+import java.nio.file.Files
+import java.util.*
 
 @Service
 class ImageServiceImpl(
         @Value("\${image.upload.dir}")
-        private var imageDirPath: String
+        private var imageDirPath: String,
+
+        private var imageFileRepository: ImageFileRepository
 ): ImageService {
 
     override fun getImage(imageName: String): ByteArray {
@@ -22,5 +30,27 @@ class ImageServiceImpl(
         val inputStream: InputStream = FileInputStream(file)
 
         return IOUtils.toByteArray(inputStream)
+    }
+
+    override fun saveImageFile(post: Post, imageFile: Array<MultipartFile>?) {
+        imageFile?.let {
+            for (file in imageFile) {
+                val fileName = UUID.randomUUID().toString()
+                imageFileRepository.save(ImageFile(
+                        post = post,
+                        fileName = fileName
+                ))
+                file.transferTo(File(imageDirPath, fileName))
+            }
+        }
+    }
+
+    override fun deleteImageFile(post: Post, imageFile: List<ImageFile>?) {
+        imageFile?.let {
+            for (image in imageFile) {
+                Files.delete(File(imageDirPath, image.fileName).toPath())
+            }
+            imageFileRepository.deleteByPost(post)
+        }
     }
 }
