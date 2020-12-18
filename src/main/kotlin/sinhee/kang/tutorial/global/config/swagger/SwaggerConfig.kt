16f3dc9f30d58f19dc9eaacd512914b9640ea8ce
kangsinhee <rkdtlsgml40@naver.com>
2020-12-18 16:57:19 +0900

    Update SwaggerConfig.kt

--- src/main/kotlin/sinhee/kang/tutorial/global/config/swagger/SwaggerConfig.kt
+++ src/main/kotlin/sinhee/kang/tutorial/global/config/swagger/SwaggerConfig.kt
@@ -2,91 +2,62 @@ package sinhee.kang.tutorial.global.config.swagger
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.RequestMethod
+import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.builders.ResponseMessageBuilder
+import springfox.documentation.schema.ModelRef
 import springfox.documentation.service.ApiInfo
 import springfox.documentation.service.Contact
+import springfox.documentation.service.ResponseMessage
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 import java.util.*
-
+import kotlin.collections.ArrayList
 
 @Configuration
 @EnableSwagger2
 class SwaggerConfig {
-    private lateinit var group: String
     private lateinit var title: String
 
     @Bean
     fun authInfo(): Docket {
-        group = "auth"
-        title = "tut_kotlin_springboot $group"
+        title = "tut_kotlin_springboot"
 
-        return Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
-                .groupName(group)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("sinhee.kang.tutorial.domain.$group"))
-                .paths(PathSelectors.ant("/$group/controller/**"))
+        val messageBuilder: MutableList<ResponseMessage> = ArrayList()
+        messageBuilder.add(ResponseMessageBuilder()
+                .code(500)
+                .message("Internal Server Error")
+                .responseModel(ModelRef("Error"))
                 .build()
-                .apiInfo(apiInfo(title, group))
+        )
+        messageBuilder.add(ResponseMessageBuilder()
+                .code(400)
+                .message("Bad Request")
+                .build()
+        )
+        messageBuilder.add(ResponseMessageBuilder()
+                .code(404)
+                .message("Not Found")
+                .build()
+        )
+        return Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo(title))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, messageBuilder)
     }
 
-    @Bean
-    fun fileInfo(): Docket {
-        group = "file"
-        title = "tut_kotlin_springboot $group"
-
-        return Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
-                .groupName(group)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("sinhee.kang.tutorial.domain.$group"))
-                .paths(PathSelectors.ant("/$group/controller/**"))
+    private fun apiInfo(title: String): ApiInfo {
+        return ApiInfoBuilder()
+                .title(title)
+                .version("v2")
+                .description("tut_Kotlin_Springboot API docs with Swagger2")
                 .build()
-                .apiInfo(apiInfo(title, group))
-    }
-
-    @Bean
-    fun postInfo(): Docket {
-        group = "post"
-        title = "tut_kotlin_springboot $group"
-
-        return Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
-                .groupName(group)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("sinhee.kang.tutorial.domain.$group"))
-                .paths(PathSelectors.ant("/$group/controller/**"))
-                .build()
-                .apiInfo(apiInfo(title, group))
-    }
-
-    @Bean
-    fun userInfo(): Docket {
-        group = "user"
-        title = "tut_kotlin_springboot $group"
-
-        return Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
-                .groupName(group)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("sinhee.kang.tutorial.domain.$group"))
-                .paths(PathSelectors.ant("/$group/controller/**"))
-                .build()
-                .apiInfo(apiInfo(title, group))
-    }
-
-    private fun apiInfo(title: String, group: String): ApiInfo {
-        return ApiInfo(
-                title,
-                "tut_kotlin_springboot API Docs made by Swagger2",
-                group,
-                "",
-                Contact("", "", ""),
-                "Licenses",
-                "",
-                ArrayList())
     }
 }
