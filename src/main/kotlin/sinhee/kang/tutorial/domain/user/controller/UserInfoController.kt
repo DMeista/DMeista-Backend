76435d459288335d461706b74edb8147ad943e25
kangsinhee <rkdtlsgml40@naver.com>
2020-12-18 16:57:36 +0900

    Update UserInfoController.kt

--- src/main/kotlin/sinhee/kang/tutorial/domain/user/controller/UserInfoController.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/user/controller/UserInfoController.kt
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.*
 import sinhee.kang.tutorial.domain.user.dto.response.UserInfoResponse
 import sinhee.kang.tutorial.domain.user.service.userInfo.UserInfoService
 
-
 @RestController
-@RequestMapping("/")
+@RequestMapping("/users")
 class UserInfoController(
         private var userInfoService: UserInfoService
 ) {
