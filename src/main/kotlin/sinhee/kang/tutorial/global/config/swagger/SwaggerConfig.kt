11ab53f63cf08e6a5a487523457cf01944b0e16d
kangsinhee <rkdtlsgml40@naver.com>
2020-12-21 11:01:10 +0900

    Update SwaggerConfig.kt

--- src/main/kotlin/sinhee/kang/tutorial/global/config/swagger/SwaggerConfig.kt
+++ src/main/kotlin/sinhee/kang/tutorial/global/config/swagger/SwaggerConfig.kt
@@ -9,12 +9,10 @@ import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.builders.ResponseMessageBuilder
 import springfox.documentation.schema.ModelRef
 import springfox.documentation.service.ApiInfo
-import springfox.documentation.service.Contact
 import springfox.documentation.service.ResponseMessage
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
-import java.util.*
 import kotlin.collections.ArrayList
 
 @Configuration
