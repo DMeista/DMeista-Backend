9b9e7cdb8b70c4dc95aa61d6e4cc476cef63889b
kangsinhee <rkdtlsgml40@naver.com>
2020-12-21 15:20:35 +0900

    [UPDATE] SignIn Test

--- src/test/kotlin/sinhee/kang/tutorial/TutorialApplicationTests.kt
+++ src/test/kotlin/sinhee/kang/tutorial/TutorialApplicationTests.kt
@@ -1,5 +1,7 @@
 package sinhee.kang.tutorial
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import org.junit.Test
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.TestMethodOrder
@@ -7,12 +9,17 @@ import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import sinhee.kang.tutorial.domain.auth.dto.request.SignInRequest
+import sinhee.kang.tutorial.domain.user.domain.user.repository.UserRepository
 import sinhee.kang.tutorial.infra.redis.EmbeddedRedisConfig
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(classes = [TutorialApplication::class, EmbeddedRedisConfig::class],
@@ -23,10 +30,26 @@ import sinhee.kang.tutorial.infra.redis.EmbeddedRedisConfig
 class TutorialApplicationTests {
     @Autowired
     private lateinit var mvc: MockMvc
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
 
     @Test
     fun test() {
         mvc.perform(MockMvcRequestBuilders.get("/posts"))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun signIn() {
+        val signInRequest = SignInRequest(email = "rkdtlsgml50@naver.com", password = "1234")
+        mvc.perform(MockMvcRequestBuilders.post("/auth")
+                .content(ObjectMapper()
+                        .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                        .writeValueAsString(signInRequest))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk).andDo(print())
+                .andReturn()
     }
 }
