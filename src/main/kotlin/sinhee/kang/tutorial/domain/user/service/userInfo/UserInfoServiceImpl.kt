f1a766a2ba7bb6c0e4cf57790b13ccc6ef68026b
kangsinhee <rkdtlsgml40@naver.com>
2020-12-21 11:04:50 +0900

    [ADD] Add View Service

--- src/main/kotlin/sinhee/kang/tutorial/domain/user/service/userInfo/UserInfoServiceImpl.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/user/service/userInfo/UserInfoServiceImpl.kt
@@ -2,6 +2,7 @@ package sinhee.kang.tutorial.domain.user.service.userInfo
 
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import sinhee.kang.tutorial.domain.post.domain.view.repository.ViewRepository
 import sinhee.kang.tutorial.domain.post.dto.response.PostResponse
 import sinhee.kang.tutorial.domain.user.domain.user.repository.UserRepository
 import sinhee.kang.tutorial.domain.user.dto.response.UserInfoResponse
@@ -9,7 +10,8 @@ import sinhee.kang.tutorial.global.config.security.exception.UserNotFoundExcepti
 
 @Service
 class UserInfoServiceImpl(
-        private var userRepository: UserRepository
+        private var userRepository: UserRepository,
+        private var viewRepository: ViewRepository
 ): UserInfoService {
 
     override fun getUserInfo(pageable: Pageable, nickname: String): UserInfoResponse? {
@@ -18,12 +20,13 @@ class UserInfoServiceImpl(
 
         val postResponse: MutableList<PostResponse> = ArrayList()
         for(post in user.postList.reversed()) {
+            val checkedUser = viewRepository.findByPost(post)
             postResponse.add(PostResponse(
                     id = post.postId,
                     title = post.title,
                     content = post.content,
                     author = post.author,
-                    view = post.view,
+                    view = checkedUser.count(),
                     createdAt = post.createdAt
             ))
         }
