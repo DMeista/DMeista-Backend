5b8ee50aa77f4c58e13280cd255a4645cfbbac33
kangsinhee <rkdtlsgml40@naver.com>
2020-12-23 01:38:02 +0900

    [ADD] Upload Post Test

--- src/test/kotlin/sinhee/kang/tutorial/post/PostApiTest.kt
+++ src/test/kotlin/sinhee/kang/tutorial/post/PostApiTest.kt
@@ -3,6 +3,7 @@ package sinhee.kang.tutorial.post
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import org.assertj.core.api.ArraySortedAssert
+import org.junit.Before
 import org.junit.FixMethodOrder
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.MvcResult
@@ -21,9 +23,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import sinhee.kang.tutorial.TutorialApplication
+import sinhee.kang.tutorial.domain.auth.dto.request.SignInRequest
+import sinhee.kang.tutorial.domain.auth.dto.response.TokenResponse
 import sinhee.kang.tutorial.domain.post.domain.post.repository.PostRepository
 import sinhee.kang.tutorial.domain.post.dto.response.PostContentResponse
 import sinhee.kang.tutorial.domain.post.dto.response.PostListResponse
+import sinhee.kang.tutorial.domain.user.domain.user.repository.UserRepository
 import sinhee.kang.tutorial.infra.redis.EmbeddedRedisConfig
 
 @RunWith(SpringRunner::class)
@@ -35,13 +40,19 @@ class PostApiTest {
     @Autowired
     private lateinit var mvc: MockMvc
     @Autowired
+    private lateinit var userRepository: UserRepository
+    @Autowired
     private lateinit var postRepository: PostRepository
+    @Autowired
+    private lateinit var passwordEncoder: PasswordEncoder
     @Autowired
     private lateinit var objectMapper: ObjectMapper
 
+
+
     @Test
     @Throws
-    fun getAllPostListTest() {
+    fun getAllHashTagPostList_LoadTest() {
         val post: String = mvc.perform(get("/posts"))
                 .andDo(print())
                 .andExpect(status().isOk)
@@ -49,14 +60,15 @@ class PostApiTest {
         val response: PostListResponse = objectMapper
                 .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
                 .readValue(post, PostListResponse::class.java)
-        assert(response.totalItems == 4)
+        assert(response.totalItems == postRepository.findAll().count())
     }
 
 
     @Test
     @Throws
-    fun getTagPostListTest() {
-        val post: String = mvc.perform(get("/posts?tags=안경"))
+    fun getHashTagGroupPostList_LoadTest() {
+        val post: String = mvc.perform(get("/posts")
+                .param("tags", "안경"))
                 .andDo(print())
                 .andExpect(status().isOk)
                 .andReturn().response.contentAsString
@@ -83,8 +95,19 @@ class PostApiTest {
     @Test
     @Throws
     fun uploadPostTest() {
-        
+        val accessToken = accessKey()
+        val upload = mvc.perform(post("/posts")
+                .header("Authorization", "Bearer $accessToken")
+                .param("title", "junit4 test")
+                .param("content", "content")
+                .param("tags", "junit4, freak"))
+                .andDo(print())
+                .andExpect(status().isOk)
+                .andReturn().response.contentAsString
+        println(upload)
+
     }
+
 
 
     @Throws
@@ -96,5 +119,27 @@ class PostApiTest {
                 .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk)
                 .andReturn()
+    }
+
+
+    @Throws
+    private fun signIn(): MvcResult {
+        val signInRequest = SignInRequest("rkdtlsgml50@naver.com", "1234")
+        return mvc.perform(post("/auth")
+                .content(ObjectMapper()
+                        .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                        .writeValueAsString(signInRequest))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk)
+                .andReturn()
+    }
+
+
+    private fun accessKey(): String {
+        val content: String = signIn().response.contentAsString
+        val response: TokenResponse = ObjectMapper()
+                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                .readValue(content, TokenResponse::class.java)
+        return response.accessToke
     }
 }
