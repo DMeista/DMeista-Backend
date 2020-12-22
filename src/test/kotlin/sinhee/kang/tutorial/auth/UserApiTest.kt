8e7d266e6e5b404c0ecc8d3466e8a9c642c44bfb
kangsinhee <rkdtlsgml40@naver.com>
2020-12-22 14:41:14 +0900

    [ADD] nicknameVerifyTest

--- src/test/kotlin/sinhee/kang/tutorial/auth/UserApiTest.kt
+++ src/test/kotlin/sinhee/kang/tutorial/auth/UserApiTest.kt
@@ -3,9 +3,7 @@ package sinhee.kang.tutorial.auth
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import org.junit.Test
-import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.Tag
-import org.junit.jupiter.api.TestMethodOrder
+import org.junit.jupiter.api.*
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -16,8 +14,14 @@ import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import sinhee.kang.tutorial.TutorialApplication
+import sinhee.kang.tutorial.domain.auth.domain.verification.EmailVerification
+import sinhee.kang.tutorial.domain.auth.domain.verification.enums.EmailVerificationStatus
 import sinhee.kang.tutorial.domain.auth.domain.verification.repository.EmailVerificationRepository
 import sinhee.kang.tutorial.domain.auth.dto.request.VerifyCodeRequest
 import sinhee.kang.tutorial.domain.user.domain.user.repository.UserRepository
@@ -38,15 +42,37 @@ class UserApiTest {
     @Autowired
     private lateinit var passwordEncoder: PasswordEncoder
 
-    @Tag("First")
     @Test
     @Throws(Exception::class)
     fun emailVerifyTest() {
+        sendEmail()
         val request = VerifyCodeRequest("rkdtlsgml50@naver.com", "ASD123")
-        requestMvc(MockMvcRequestBuilders.post("/users/email/verify/signup"), request)
-
+        requestMvc(put("/users/email/verify"), request)
     }
 
+
+    @Test
+    @Throws(Exception::class)
+    fun signUp() {
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun nicknameVerifyTest() {
+        mvc.perform(get("/users/nickname?nickname=sinheekang"))
+                .andExpect(status().isOk)
+    }
+
+
+    @Throws(Exception::class)
+    fun sendEmail() {
+        emailVerificationRepository.save(EmailVerification(
+                email = "rkdtlsgml50@naver.com",
+                authCode = "ASD123",
+                status = EmailVerificationStatus.UNVERIFID
+        ))
+    }
 
     @Throws(Exception::class)
     private fun requestMvc(method: MockHttpServletRequestBuilder, obj: Any) {
