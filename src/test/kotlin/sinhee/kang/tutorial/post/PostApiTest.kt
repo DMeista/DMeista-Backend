b491b8770f4889df7dd8a961a32042b77d44cbc9
kangsinhee <rkdtlsgml40@naver.com>
2020-12-22 22:04:23 +0900

    [ADD]

--- src/test/kotlin/sinhee/kang/tutorial/post/PostApiTest.kt
+++ src/test/kotlin/sinhee/kang/tutorial/post/PostApiTest.kt
@@ -1,4 +1,56 @@
 package sinhee.kang.tutorial.post
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import org.junit.FixMethodOrder
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MvcResult
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import sinhee.kang.tutorial.TutorialApplication
+import sinhee.kang.tutorial.domain.post.domain.post.repository.PostRepository
+import sinhee.kang.tutorial.infra.redis.EmbeddedRedisConfig
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(classes = [TutorialApplication::class, EmbeddedRedisConfig::class],
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class PostApiTest {
+    @Autowired
+    private lateinit var mvc: MockMvc
+    @Autowired
+    private lateinit var postRepository: PostRepository
+
+    @Test
+    @Throws
+    fun getAllPostListTest() {
+        mvc.perform(get("/posts"))
+                .andDo(print())
+                .andExpect(status().isOk)
+    }
+
+
+    @Throws
+    private fun requestMvc(method: MockHttpServletRequestBuilder, obj: Any) {
+        mvc.perform(method
+                .content(ObjectMapper()
+                        .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                        .writeValueAsString(obj))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk)
+                .andReturn()
+    }
 }
