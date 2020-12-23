5a7220985550103c09204e9c7dbcb06e3a59c130
kangsinhee <rkdtlsgml40@naver.com>
2020-12-23 14:45:51 +0900

    [ADD] emojiTest

--- src/test/kotlin/sinhee/kang/tutorial/post/EmojiApiTest.kt
+++ src/test/kotlin/sinhee/kang/tutorial/post/EmojiApiTest.kt
@@ -1,4 +1,75 @@
 package sinhee.kang.tutorial.post
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import org.junit.FixMethodOrder
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MvcResult
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import sinhee.kang.tutorial.TutorialApplication
+import sinhee.kang.tutorial.domain.auth.dto.request.SignInRequest
+import sinhee.kang.tutorial.domain.auth.dto.response.TokenResponse
+import sinhee.kang.tutorial.domain.post.domain.post.repository.PostRepository
+import sinhee.kang.tutorial.domain.user.domain.user.repository.UserRepository
+import sinhee.kang.tutorial.infra.redis.EmbeddedRedisConfig
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(classes = [TutorialApplication::class, EmbeddedRedisConfig::class],
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class EmojiApiTest {
+    @Autowired
+    private lateinit var mvc: MockMvc
+    @Autowired
+    private lateinit var userRepository: UserRepository
+    @Autowired
+    private lateinit var postRepository: PostRepository
+    @Autowired
+    private lateinit var passwordEncoder: PasswordEncoder
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+
+    @Test
+    @Throws
+    fun emojiTest() {
+    }
+
+
+    @Throws
+    private fun signIn(): MvcResult {
+        val signInRequest = SignInRequest("rkdtlsgml50@naver.com", "1234")
+        return mvc.perform(post("/auth")
+                .content(ObjectMapper()
+                        .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                        .writeValueAsString(signInRequest))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk)
+                .andReturn()
+    }
+
+
+    private fun accessKey(): String {
+        val content: String = signIn().response.contentAsString
+        val response: TokenResponse = ObjectMapper()
+                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                .readValue(content, TokenResponse::class.java)
+        return response.accessToke
+    }
 }
