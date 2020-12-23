3e6cacfc064df0ddf331e4fa94cdebfd96303454
kangsinhee <rkdtlsgml40@naver.com>
2020-12-23 15:53:50 +0900

    [UPDATE] Initial value

--- src/main/kotlin/sinhee/kang/tutorial/domain/post/dto/response/PostEmojiListResponse.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/post/dto/response/PostEmojiListResponse.kt
@@ -1,6 +1,6 @@
 package sinhee.kang.tutorial.domain.post.dto.response
 
 class PostEmojiListResponse (
-        var totalEmoji: Int,
-        var applicationResponses: MutableList<EmojiResponse>
+        var totalEmoji: Int = 0,
+        var applicationResponses: MutableList<EmojiResponse> = arrayListOf()
 )
