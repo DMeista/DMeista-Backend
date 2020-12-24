22f83466ab2aea1a09dffe1ceb10f804d62e5c3b
kangsinhee <rkdtlsgml40@naver.com>
2020-12-25 01:53:30 +0900

    [UPDATE] Unified Token func

--- src/test/kotlin/sinhee/kang/tutorial/post/EmojiApiTest.kt
+++ src/test/kotlin/sinhee/kang/tutorial/post/EmojiApiTest.kt
@@ -2,6 +2,8 @@ package sinhee.kang.tutorial.post
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import org.junit.After
+import org.junit.Before
 import org.junit.FixMethodOrder
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -10,9 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.MvcResult
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
@@ -25,6 +29,8 @@ import sinhee.kang.tutorial.domain.post.domain.emoji.repository.EmojiRepository
 import sinhee.kang.tutorial.domain.post.domain.post.repository.PostRepository
 import sinhee.kang.tutorial.domain.post.dto.response.EmojiResponse
 import sinhee.kang.tutorial.domain.post.dto.response.PostEmojiListResponse
+import sinhee.kang.tutorial.domain.user.domain.user.User
+import sinhee.kang.tutorial.domain.user.domain.user.repository.UserRepository
 import sinhee.kang.tutorial.infra.redis.EmbeddedRedisConfig
 
 @RunWith(SpringRunner::class)
@@ -37,10 +43,33 @@ class EmojiApiTest {
     @Autowired
     private lateinit var postRepository: PostRepository
     @Autowired
+    private lateinit var userRepository: UserRepository
+    @Autowired
+    private lateinit var passwordEncoder: PasswordEncoder
+    @Autowired
     private lateinit var emojiRepository: EmojiRepository
     @Autowired
     private lateinit var objectMapper: ObjectMapper
 
+    val testMail = "rkdtlsgml50@naver.com"
+    val passwd = "1234"
+    val username = "user"
+
+
+    @Before
+    fun setup() {
+        userRepository.save(User(
+                email = testMail,
+                password = passwordEncoder.encode(passwd),
+                nickname = username
+        ))
+    }
+
+    @After
+    fun clean() {
+        userRepository.findByNickname(username)
+                ?.let { user -> userRepository.delete(user) }
+    }
 
     @Test
     @Throws
@@ -93,9 +122,8 @@ class EmojiApiTest {
     }
 
 
-    @Throws
     private fun requestEmoji(postId: Int, status: EmojiStatus): String {
-        val accessToken = accessKey()
+        val accessToken = accessToken()
         return mvc.perform(post("/posts/$postId/emoji")
                 .header("Authorization", "Bearer $accessToken")
                 .param("status", "$status"))
@@ -105,7 +133,6 @@ class EmojiApiTest {
     }
 
 
-    @Throws
     private fun requestEmojiList(postId: Int): PostEmojiListResponse {
         return mvc.perform(get("/posts/$postId/emoji"))
                 .andDo(print())
@@ -118,9 +145,8 @@ class EmojiApiTest {
     }
 
 
-    @Throws
     private fun uploadPost(): Int {
-        val accessToken = accessKey()
+        val accessToken = accessToken()
         return Integer.parseInt(mvc.perform(post("/posts")
                 .header("Authorization", "Bearer $accessToken")
                 .param("title", "title")
@@ -132,24 +158,28 @@ class EmojiApiTest {
     }
 
 
-    @Throws
-    private fun signIn(): MvcResult {
-        val signInRequest = SignInRequest("rkdtlsgml50@naver.com", "1234")
-        return mvc.perform(post("/auth")
-                .content(ObjectMapper()
-                        .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-                        .writeValueAsString(signInRequest))
-                .contentType(MediaType.APPLICATION_JSON_VALUE))
+    private fun requestMvc(method: MockHttpServletRequestBuilder, obj: Any? = null): String {
+        return mvc.perform(
+                method
+                        .content(ObjectMapper()
+                                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                                .writeValueAsString(obj))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk)
-                .andReturn()
+                .andReturn().response.contentAsString
     }
 
 
-    private fun accessKey(): String {
-        val content: String = signIn().response.contentAsString
-        val response: TokenResponse = ObjectMapper()
+    private fun accessToken(): String {
+        val content = requestMvc(post("/auth"), SignInRequest(testMail, passwd))
+        val response = mappingResponse(content, TokenResponse::class.java) as TokenResponse
+        return response.accessToken
+    }
+
+
+    private fun mappingResponse(obj: String, cls: Class<*>): Any {
+        return objectMapper
                 .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-                .readValue(content, TokenResponse::class.java)
-        return response.accessToke
+                .readValue(obj, cls)
     }
 }
