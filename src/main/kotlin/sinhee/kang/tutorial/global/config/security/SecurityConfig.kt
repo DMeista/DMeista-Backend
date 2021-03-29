8add0ad4a3e246b80e120666a3611202089e9039
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-03-30 00:28:34 +0900

    [ADD] - Apply Spring Security CORS Config

--- src/main/kotlin/sinhee/kang/tutorial/global/config/security/SecurityConfig.kt
+++ src/main/kotlin/sinhee/kang/tutorial/global/config/security/SecurityConfig.kt
@@ -9,11 +9,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.util.matcher.RequestMatcher
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.CorsUtils
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import sinhee.kang.tutorial.global.config.security.exception.ExceptionConfigurer
 import sinhee.kang.tutorial.global.config.security.jwt.JwtConfigurer
 import sinhee.kang.tutorial.global.config.security.jwt.JwtTokenProvider
 import sinhee.kang.tutorial.global.config.requestLog.RequestLogConfigurer
 import sinhee.kang.tutorial.infra.api.slack.SlackSenderManager
+import javax.servlet.http.HttpServletRequest
 
 @Configuration
 @EnableWebSecurity
@@ -29,29 +35,46 @@ class SecurityConfig(
 
     override fun configure(http: HttpSecurity) {
         http
-                .csrf().disable()
-                .formLogin().disable()
-                .headers()
-                .frameOptions()
+            .csrf().disable()
+            .cors().and()
+            .formLogin()
+                .disable()
+            .headers()
+            .frameOptions()
                 .disable().and()
-                .sessionManagement()
+            .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests()
+            .authorizeRequests()
+                .requestMatchers(RequestMatcher {
+                        request: HttpServletRequest -> CorsUtils.isPreFlightRequest(request)
+                }).permitAll()
                 .antMatchers("/swagger-ui.html").permitAll()
                 .antMatchers("/auth").permitAll()
                 .antMatchers("/post").permitAll()
                 .antMatchers("/users").permitAll()
                 .antMatchers("/users/password").permitAll()
                 .antMatchers("/users/email/password/verify").permitAll()
-                .antMatchers("/users/email/verify").permitAll().and()
-                .apply(JwtConfigurer(jwtTokenProvider)).and()
-                .apply(ExceptionConfigurer(slackSenderManager)).and()
-                .apply(RequestLogConfigurer())
-
+                .antMatchers("/users/email/verify").permitAll()
+            .anyRequest().authenticated().and()
+            .apply(JwtConfigurer(jwtTokenProvider)).and()
+            .apply(ExceptionConfigurer(slackSenderManager)).and()
+            .apply(RequestLogConfigurer())
     }
 
     @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return BCryptPasswordEncoder()
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val configuration = CorsConfiguration().apply {
+            addAllowedOrigin("*")
+            addAllowedHeader("*")
+            addAllowedMethod("GET, POST, PUT, PATCH")
+            allowCredentials = true
+            maxAge = 3600L
+        }
+
+        UrlBasedCorsConfigurationSource().registerCorsConfiguration("/**", configuration)
+        return UrlBasedCorsConfigurationSource()
     }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
