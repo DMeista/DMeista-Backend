5b0f8073286bca9ce24e9de8712dbe0ea11dfb74
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-04-11 14:15:58 +0900

    .

--- src/main/kotlin/sinhee/kang/tutorial/global/config/WebConfig.kt
+++ src/main/kotlin/sinhee/kang/tutorial/global/config/WebConfig.kt
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig: WebMvcConfigurer {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry.addResourceHandler("/images/**")
-            .addResourceLocations("file:///home/ubuntu/resource/")
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("file:/home/ubuntu/resource/")
     }
 }
