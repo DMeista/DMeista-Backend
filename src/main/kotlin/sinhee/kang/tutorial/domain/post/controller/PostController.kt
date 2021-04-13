3dd179c4796a081286451f216bd2fb9df7623c07
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-04-14 00:27:13 +0900

    [MODIFY] - Post Controller Code Clean Up

--- src/main/kotlin/sinhee/kang/tutorial/domain/post/controller/PostController.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/post/controller/PostController.kt
@@ -3,6 +3,7 @@ package sinhee.kang.tutorial.domain.post.controller
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+
 import sinhee.kang.tutorial.domain.post.domain.emoji.enums.EmojiStatus
 import sinhee.kang.tutorial.domain.post.dto.response.EmojiResponse
 import sinhee.kang.tutorial.domain.post.dto.response.PostContentResponse
@@ -17,47 +18,46 @@ class PostController(
         private val postService: PostService,
         private val emojiService: EmojiService
 ) {
+
     @GetMapping
-    fun getAllHashTagPostList(page: Pageable, @RequestParam(defaultValue = "") tags: String?): PostListResponse {
-        return postService.getAllHashTagList(page, tags)
-    }
+    fun getAllHashTagPostList(page: Pageable, @RequestParam(defaultValue = "") tags: String?): PostListResponse =
+        postService.getAllHashTagList(page, tags)
 
 
     @GetMapping("/{postId}")
-    fun getPostContent(@PathVariable postId: Int): PostContentResponse {
-        return postService.getPostContent(postId)
-    }
+    fun getPostContent(@PathVariable postId: Int): PostContentResponse =
+        postService.getPostContent(postId)
+
 
     @PostMapping
     fun uploadPost(@RequestParam title: String,
                    @RequestParam content: String,
                    @RequestParam tags: String?,
                    @RequestParam autoTag: Boolean?,
-                   @RequestParam imageFile: Array<MultipartFile>?): Int? {
-        return postService.uploadPost(title, content, tags, autoTag?: false, imageFile)
-    }
+                   @RequestParam imageFile: Array<MultipartFile>?): Int? =
+        postService.uploadPost(title, content, tags, autoTag?: false, imageFile)
+
 
     @PatchMapping("/{postId}")
     fun editPost(@PathVariable postId: Int,
                  @RequestParam title: String,
                  @RequestParam content: String,
                  @RequestParam tags: String?,
-                 @RequestParam imageFile: Array<MultipartFile>?): Int? {
-        return postService.changePost(postId, title, content, tags, imageFile)
-    }
+                 @RequestParam imageFile: Array<MultipartFile>?): Int? =
+        postService.changePost(postId, title, content, tags, imageFile)
+
 
     @DeleteMapping("/{postId}")
-    fun deletePost(@PathVariable postId: Int) {
+    fun deletePost(@PathVariable postId: Int) =
         postService.deletePost(postId)
-    }
 
-    @PostMapping("/{postId}/emoji")
-    fun emojiPost(@PathVariable postId: Int, @RequestParam status: EmojiStatus): EmojiResponse? {
-        return emojiService.emojiService(postId, status)
-    }
 
     @GetMapping("/{postId}/emoji")
-    fun getEmojiUser(@PathVariable postId: Int): PostEmojiListResponse {
-        return emojiService.getPostEmojiUserList(postId)
-    }
+    fun getEmojiUser(@PathVariable postId: Int): PostEmojiListResponse =
+        emojiService.getPostEmojiUserList(postId)
+
+
+    @PostMapping("/{postId}/emoji")
+    fun emojiPost(@PathVariable postId: Int, @RequestParam status: EmojiStatus): EmojiResponse? =
+        emojiService.emojiService(postId, status)
 }
