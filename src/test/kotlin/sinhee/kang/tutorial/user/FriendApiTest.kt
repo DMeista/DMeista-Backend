0a10a6d77edf2e324dce691507831798a1601a40
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-04-13 14:58:12 +0900

    [FIX] - Friends API Test 전부 수정

--- src/test/kotlin/sinhee/kang/tutorial/user/FriendApiTest.kt
+++ src/test/kotlin/sinhee/kang/tutorial/user/FriendApiTest.kt
@@ -7,15 +7,12 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-
 import sinhee.kang.tutorial.ApiTest
 import sinhee.kang.tutorial.domain.auth.dto.request.SignInRequest
 import sinhee.kang.tutorial.domain.auth.dto.response.TokenResponse
@@ -24,7 +21,6 @@ import sinhee.kang.tutorial.domain.user.domain.friend.repository.FriendRepositor
 import sinhee.kang.tutorial.domain.user.domain.user.User
 import sinhee.kang.tutorial.domain.user.domain.user.repository.UserRepository
 import sinhee.kang.tutorial.domain.user.dto.response.UserListResponse
-import sinhee.kang.tutorial.global.security.exception.UserNotFoundException
 
 class FriendApiTest: ApiTest() {
     @Autowired
@@ -34,81 +30,69 @@ class FriendApiTest: ApiTest() {
     @Autowired
     private lateinit var friendRepository: FriendRepository
     @Autowired
-    private lateinit var passwordEncoder: PasswordEncoder
-    @Autowired
     private lateinit var objectMapper: ObjectMapper
 
-    private val testMail = "rkdtlsgml500@naver.com"
-    private val username = "user1"
+    private val passwordEncoder: PasswordEncoder = BCryptPasswordEncoder()
 
-    private val testMail2 = "rkdtlsgml400@naver.com"
-    private val username2 = "user2"
+    private val user1: User = User(
+        nickname = "user1",
+        email = "rkdtlsgml500@naver.com",
+        password = passwordEncoder.encode("1234")
+    )
 
-    private val passwd = "1234"
+    private val user2: User = User(
+        nickname = "user2",
+        email = "rkdtlsgml400@naver.com",
+        password =  passwordEncoder.encode("1234")
+    )
 
     private lateinit var user1Token: String
     private lateinit var user2Token: String
 
-    private lateinit var user: User
-    private lateinit var targetUser: User
-
-
     @BeforeEach
     fun setup() {
-        userRepository.save(User(
-                email = testMail,
-                password = passwordEncoder.encode(passwd),
-                nickname = username
-        ))
-        userRepository.save(User(
-                email = testMail2,
-                password = passwordEncoder.encode(passwd),
-                nickname = username2
-        ))
-        user1Token = "Bearer ${accessToken(testMail, passwd)}"
-        user2Token = "Bearer ${accessToken(testMail2, passwd)}"
-
-        user = userRepository.findByNickname(username)
-                ?: throw UserNotFoundException()
-        targetUser = userRepository.findByNickname(username2)
-                ?: throw UserNotFoundException()
+        userRepository.save(user1)
+        userRepository.save(user2)
+        user1Token = "Bearer ${accessToken(user1.email)}"
+        user2Token = "Bearer ${accessToken(user2.email)}"
     }
+
 
     @AfterEach
     fun clean() {
         friendRepository.deleteAll()
-        userRepository.findByNickname(username)
-                ?.let { user -> userRepository.delete(user) }
-        userRepository.findByNickname(username2)
-                ?.let { user -> userRepository.delete(user) }
+        userRepository.deleteAll()
     }
 
 
     @Test
     @Throws
     fun getReceiveFriendRequestTest() {
-        requestMvc(post("/users/friends/${targetUser.id}"), token = user1Token)
-        val request = mappingResponse(requestMvc(get("/users/friends/request"), token = user2Token), UserListResponse::class.java) as UserListResponse
+        requestParam(post("/users/friends"), targetName = user2.nickname, token = user1Token)
+        val request = mappingResponse(
+            requestContent(get("/users/friends/request"), token = user2Token),
+            UserListResponse::class.java
+        ) as UserListResponse
 
-        assert(request.applicationResponses[0].nickname == user.nickname)
+        assert(value = request.applicationResponses[0].nickname == user1.nickname)
     }
 
 
     @Test
     @Throws
     fun sendFriendRequestTest() {
-        requestMvc(post("/users/friends/${targetUser.id}"), token = user1Token)
-        assert(isCheckUserAndTargetUserExist(user, targetUser))
+        requestParam(post("/users/friends"), targetName = user2.nickname, token = user1Token)
+        assert(isCheckUserAndTargetUserExist(user1, user2))
     }
 
 
     @Test
     @Throws
     fun acceptFriendRequestTest() {
-        requestMvc(post("/users/friends/${targetUser.id}"), token = user1Token)
-        requestMvc(put("/users/friends/${user.id}"), token = user2Token)
+        requestParam(post("/users/friends"), targetName = user2.nickname, token = user1Token)
+        requestParam(put("/users/friends"), targetName = user1.nickname, token = user2Token)
 
-        assert(friendRepository.findByUserIdAndTargetIdAndStatus(user, targetUser, FriendStatus.ACCEPT)
+        assert(friendRepository.findByUserIdAndTargetIdAndStatus(user1, user2, FriendStatus.ACCEPT)
                 ?.let { true }
                 ?: false
         )
@@ -118,34 +102,21 @@ class FriendApiTest: ApiTest() {
     @Test
     @Throws
     fun deniedFriendRequestTest() {
-        requestMvc(post("/users/friends/${targetUser.id}"), token = user1Token)
-        requestMvc(delete("/users/friends/${user.id}"), token = user2Token)
+        requestParam(post("/users/friends"), targetName = user2.nickname, token = user1Token)
+        requestParam(delete("/users/friends"), targetName = user1.nickname, token = user2Token)
 
-        requestMvc(post("/users/friends/${targetUser.id}"), token = user1Token)
-        requestMvc(delete("/users/friends/${targetUser.id}"), token = user1Token)
-
-        assert(friendRepository.findByUserIdAndTargetId(user, targetUser)
-                ?.let { throw Exception() }
-                ?: true
-        )
+        assert(!isCheckUserAndTargetUserExist(user1, user2))
     }
 
 
     @Test
     @Throws
     fun deleteFriendTest() {
-        requestMvc(post("/users/friends/${targetUser.id}"), token = user1Token)
-        requestMvc(put("/users/friends/${user.id}"), token = user2Token)
-        requestMvc(delete("/users/friends/${user.id}"), token = user2Token)
+        requestParam(post("/users/friends"), targetName = user2.nickname, token = user1Token)
+        requestParam(put("/users/friends"), targetName = user1.nickname, token = user2Token)
+        requestParam(delete("/users/friends"), targetName = user1.nickname, token = user2Token)
 
-        requestMvc(post("/users/friends/${targetUser.id}"), token = user1Token)
-        requestMvc(put("/users/friends/${user.id}"), token = user2Token)
-        requestMvc(delete("/users/friends/${targetUser.id}"), token = user1Token)
-
-        assert(friendRepository.findByUserIdAndTargetId(user, targetUser)
-                ?.let { throw Exception() }
-                ?: true
-        )
+        assert(!isCheckUserAndTargetUserExist(user1, user2))
     }
 
 
@@ -156,25 +127,33 @@ class FriendApiTest: ApiTest() {
     }
 
 
-    private fun requestMvc(method: MockHttpServletRequestBuilder, obj: Any? = null, token: String? = ""): String {
+    private fun requestContent(method: MockHttpServletRequestBuilder, obj: Any? = null, token: String? = ""): String {
         return mvc.perform(
-                method
-                        .header("Authorization", token)
-                        .content(objectMapper
-                                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-                                .writeValueAsString(obj))
-                        .contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk)
-                .andReturn().response.contentAsString
+            method
+                .header("Authorization", token)
+                .content(ObjectMapper()
+                    .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                    .writeValueAsString(obj))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(status().isOk)
+            .andReturn().response.contentAsString
+    }
+
+    private fun requestParam(method: MockHttpServletRequestBuilder, targetName: String? = null, token: String? = ""): String {
+        return mvc.perform(
+            method
+                .header("Authorization", token)
+                .param("nickname", targetName))
+            .andExpect(status().isOk)
+            .andReturn().response.contentAsString
     }
 
 
-    private fun accessToken(email: String, passwd: String): String {
-        val content = requestMvc(post("/auth"), SignInRequest(email, passwd))
+    private fun accessToken(email: String): String {
+        val content = requestContent(post("/auth"), SignInRequest(email, "1234"))
         val response = mappingResponse(content, TokenResponse::class.java) as TokenResponse
         return response.accessToken
     }
-
 
     private fun mappingResponse(obj: String, cls: Class<*>): Any {
         return objectMapper
