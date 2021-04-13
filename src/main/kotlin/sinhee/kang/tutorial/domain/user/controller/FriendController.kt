9f70cf3ebd95500c97bf1ee5ba2dfbfa18d75943
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-04-13 14:57:12 +0900

    [MODIFY] - Friends Controller Param 수정

--- src/main/kotlin/sinhee/kang/tutorial/domain/user/controller/FriendController.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/user/controller/FriendController.kt
@@ -1,7 +1,6 @@
 package sinhee.kang.tutorial.domain.user.controller
 
 import org.springframework.data.domain.Pageable
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import sinhee.kang.tutorial.domain.user.dto.response.UserListResponse
 import sinhee.kang.tutorial.domain.user.service.friend.FriendService
@@ -14,23 +13,22 @@ class FriendController(
 
     @GetMapping("/friends")
     fun getFriendsList(pageable: Pageable,
-                       @RequestParam(required = false) nickname: String?): UserListResponse? {
-        return friendService.getFriendList(nickname)
-    }
+                       @RequestParam(required = false) nickname: String?): UserListResponse? =
+        friendService.getFriendList(nickname)
 
     @GetMapping("/friends/request")
     fun receiveFriendRequestList(pageable: Pageable): UserListResponse? =
-        return friendService.receiveFriendRequestList(pageable)
+        friendService.receiveFriendRequestList(pageable)
 
     @PostMapping("/friends")
-    fun sendFriendRequest(@RequestParam username: String) =
-        friendService.sendFriendRequest(username)
+    fun sendFriendRequest(@RequestParam nickname: String) =
+        friendService.sendFriendRequest(nickname)
 
     @PutMapping("/friends")
-    fun acceptFriendRequest(@RequestParam username: String) =
-        friendService.acceptFriendRequest(username)
+    fun acceptFriendRequest(@RequestParam nickname: String) =
+        friendService.acceptFriendRequest(nickname)
 
     @DeleteMapping("/friends")
-    fun deniedFriendRequest(@RequestParam username: String) =
-        friendService.deleteFriend(username)
+    fun deniedFriendRequest(@RequestParam nickname: String) =
+        friendService.deleteFriend(nickname)
 }
