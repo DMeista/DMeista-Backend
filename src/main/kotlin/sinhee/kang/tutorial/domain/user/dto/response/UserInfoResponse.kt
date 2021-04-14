7dc365e20e0667281aaac3c77496436a0f191310
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-04-14 19:28:01 +0900

    [MODIFY] - UserInfo Response DateTime 포맷 지정

--- src/main/kotlin/sinhee/kang/tutorial/domain/user/dto/response/UserInfoResponse.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/user/dto/response/UserInfoResponse.kt
@@ -1,12 +1,16 @@
 package sinhee.kang.tutorial.domain.user.dto.response
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import sinhee.kang.tutorial.domain.post.dto.response.PostResponse
 import java.time.LocalDateTime
 
 data class UserInfoResponse(
-        var username: String,
-        var email: String,
-        var createdAt: LocalDateTime,
+        var username: String = "",
 
-        var postList: MutableList<PostResponse>
+        var email: String = "",
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+        var createdAt: LocalDateTime? = null,
+
+        var postList: MutableList<PostResponse> = mutableListOf()
 )
