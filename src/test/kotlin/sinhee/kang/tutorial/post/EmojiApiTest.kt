8e982ab43f394706d9d0a29b7ae19641ad9912fd
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-04-14 19:33:20 +0900

    [REFACT] - Emoji Api TestCode Refactoring

--- src/test/kotlin/sinhee/kang/tutorial/post/EmojiApiTest.kt
+++ src/test/kotlin/sinhee/kang/tutorial/post/EmojiApiTest.kt
@@ -1,153 +1,89 @@
 package sinhee.kang.tutorial.post
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.MediaType
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 import sinhee.kang.tutorial.ApiTest
-import sinhee.kang.tutorial.domain.auth.dto.request.SignInRequest
-import sinhee.kang.tutorial.domain.auth.dto.response.TokenResponse
+import sinhee.kang.tutorial.TokenType
 import sinhee.kang.tutorial.domain.post.domain.emoji.enums.EmojiStatus
 import sinhee.kang.tutorial.domain.post.domain.emoji.repository.EmojiRepository
 import sinhee.kang.tutorial.domain.post.domain.post.repository.PostRepository
 import sinhee.kang.tutorial.domain.post.dto.response.EmojiResponse
-import sinhee.kang.tutorial.domain.post.dto.response.PostEmojiListResponse
 import sinhee.kang.tutorial.domain.user.domain.user.User
 import sinhee.kang.tutorial.domain.user.domain.user.repository.UserRepository
 
 class EmojiApiTest: ApiTest() {
     @Autowired
-    private lateinit var mvc: MockMvc
-    @Autowired
     private lateinit var postRepository: PostRepository
     @Autowired
     private lateinit var userRepository: UserRepository
     @Autowired
-    private lateinit var passwordEncoder: PasswordEncoder
-    @Autowired
     private lateinit var emojiRepository: EmojiRepository
-    @Autowired
-    private lateinit var objectMapper: ObjectMapper
 
-    private val testMail = "rkdtlsgml500@naver.com"
-    private val passwd = "1234"
-    private val username = "user"
+    private val user: User = User(
+        nickname = "user",
+        email = "rkdtlsgml40@dsm.hs.kr",
+        password = passwordEncoder.encode("1234")
+    )
 
+    private lateinit var accessToken: String
 
     @BeforeEach
     fun setup() {
-        userRepository.save(User(
-                email = testMail,
-                password = passwordEncoder.encode(passwd),
-                nickname = username
-        ))
+        userRepository.save(user)
+        accessToken = "Bearer ${getToken(TokenType.ACCESS, user.email, "1234")}"
     }
 
     @AfterEach
     fun clean() {
-        userRepository.findByNickname(username)
-                ?.let { user -> userRepository.delete(user) }
+        emojiRepository.deleteAll()
+        postRepository.deleteAll()
+        userRepository.deleteAll()
     }
 
 
     @Test
     @Throws
     fun addEmojiTest() {
-        val post = uploadPost()
-        requestEmoji(post, EmojiStatus.LIKE)
-                .let { emoji ->
-                    val response = mappingResponse(emoji, EmojiResponse::class.java) as EmojiResponse
-                    assert(post == response.postId)
-                }
+        val postId = generatePost(token = accessToken)
+        val emoji = requestEmoji(postId, EmojiStatus.LIKE)
 
-        emojiRepository.deleteAll()
-        postRepository.deleteById(post)
+        val response = mappingResponse(emoji, EmojiResponse::class.java) as EmojiResponse
+        assert(response.emojiStatus == EmojiStatus.LIKE)
     }
 
 
     @Test
     @Throws
     fun changeEmojiTest() {
-        val post = uploadPost()
-        requestEmoji(post, EmojiStatus.LIKE)
-        requestEmoji(post, EmojiStatus.NICE)
+        val postId = generatePost(token = accessToken)
+        requestEmoji(postId, EmojiStatus.LIKE)
+        requestEmoji(postId, EmojiStatus.NICE)
+        val emoji = requestEmoji(postId, EmojiStatus.FUN)
 
-        emojiRepository.deleteAll()
-        postRepository.deleteById(post)
+        val response = mappingResponse(emoji, EmojiResponse::class.java) as EmojiResponse
+        assert(response.emojiStatus == EmojiStatus.FUN)
     }
 
 
     @Test
     @Throws
     fun removeEmojiTest() {
-        val post = uploadPost()
-        requestEmoji(post, EmojiStatus.LIKE)
-        requestEmoji(post, EmojiStatus.LIKE)
-
-        postRepository.deleteById(post)
+        val postId = generatePost(token = accessToken)
+        requestEmoji(postId, EmojiStatus.SAD)
+        requestEmoji(postId, EmojiStatus.SAD)
     }
 
 
-    private fun uploadPost(): Int {
-        val accessToken = accessToken()
-        return Integer.parseInt(mvc.perform(post("/posts")
-                .header("Authorization", "Bearer $accessToken")
-                .param("title", "title")
-                .param("content", "content")
-                .param("tags", "tags"))
-                .andExpect(status().isOk)
-                .andReturn().response.contentAsString)
-    }
-
-
-    private fun requestEmoji(postId: Int, status: EmojiStatus): String {
-        val accessToken = accessToken()
-        return mvc.perform(post("/posts/$postId/emoji")
-                .header("Authorization", "Bearer $accessToken")
+    private fun requestEmoji(postId: Int, status: EmojiStatus): String =
+        mvc.perform(
+            post("/posts/$postId/emoji")
+                .header("Authorization", accessToken)
                 .param("status", "$status"))
-                .andExpect(status().isOk)
-                .andReturn().response.contentAsString
-    }
-
-
-    private fun requestEmojiList(postId: Int): PostEmojiListResponse {
-        return requestMvc(get("/posts/$postId/emoji"))
-                .let { mappingResponse(it, PostEmojiListResponse::class.java) as PostEmojiListResponse }
-    }
-
-
-    private fun requestMvc(method: MockHttpServletRequestBuilder, obj: Any? = null): String {
-        return mvc.perform(
-                method
-                        .content(ObjectMapper()
-                                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-                                .writeValueAsString(obj))
-                        .contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk)
-                .andReturn().response.contentAsString
-    }
-
-
-    private fun accessToken(): String {
-        val content = requestMvc(post("/auth"), SignInRequest(testMail, passwd))
-        val response = mappingResponse(content, TokenResponse::class.java) as TokenResponse
-        return response.accessToken
-    }
-
-
-    private fun mappingResponse(obj: String, cls: Class<*>): Any {
-        return objectMapper
-                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-                .readValue(obj, cls)
-    }
+            .andExpect(status().isOk)
+            .andReturn().response.contentAsString
 }
