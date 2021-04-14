4c015b2ef9e2663eea039d949d7f87d81b189457
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-04-14 19:32:15 +0900

    [REFACT] - User Api TestCode Refactoring

--- src/test/kotlin/sinhee/kang/tutorial/auth/UserApiTest.kt
+++ src/test/kotlin/sinhee/kang/tutorial/auth/UserApiTest.kt
@@ -1,73 +1,71 @@
 package sinhee.kang.tutorial.auth
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.MediaType
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 import sinhee.kang.tutorial.ApiTest
+import sinhee.kang.tutorial.TokenType
 import sinhee.kang.tutorial.domain.auth.domain.verification.EmailVerification
 import sinhee.kang.tutorial.domain.auth.domain.verification.enums.EmailVerificationStatus
 import sinhee.kang.tutorial.domain.auth.domain.verification.repository.EmailVerificationRepository
 import sinhee.kang.tutorial.domain.auth.dto.request.*
-import sinhee.kang.tutorial.domain.auth.dto.response.TokenResponse
 import sinhee.kang.tutorial.domain.user.domain.user.User
 import sinhee.kang.tutorial.domain.user.domain.user.repository.UserRepository
+import sinhee.kang.tutorial.domain.user.dto.response.UserInfoResponse
 
 class UserApiTest: ApiTest() {
-    @Autowired
-    private lateinit var mvc: MockMvc
     @Autowired
     private lateinit var emailVerificationRepository: EmailVerificationRepository
     @Autowired
     private lateinit var userRepository: UserRepository
-    @Autowired
-    private lateinit var passwordEncoder: PasswordEncoder
-    @Autowired
-    private lateinit var objectMapper: ObjectMapper
 
-    private val testMail = "rkdtlsgml500@naver.com"
-    private val passwd = "1234"
-    private val username = "user"
+    private val user: User = User(
+        nickname = "user",
+        email = "rkdtlsgml40@dsm.hs.kr",
+        password = passwordEncoder.encode("1234")
+    )
+
+    @BeforeEach
+    fun setup() {
+        emailVerify()
+    }
 
 
     @AfterEach
     fun clean() {
         emailVerificationRepository.deleteAll()
-        userRepository.findByNickname(username)
-                ?.let { userRepository.delete(it) }
+        userRepository.deleteAll()
     }
 
 
     @Test
     @Throws
     fun signUpTest() {
-        emailVerify()
-        val request = SignUpRequest(testMail, passwordEncoder.encode(passwd), username)
-        requestMvc(post("/users"), request)
+        requestBody(post("/users"), SignUpRequest(user.email, "1234", user.nickname))
+        userRepository.findByEmail(user.email)
+            ?.let { assert(it.nickname == user.nickname) }
+            ?: throw Exception()
     }
 
 
     @Test
     @Throws
     fun changePasswordTest() {
-        userRepository.save(User(
-                email = testMail,
-                password = passwordEncoder.encode(passwd),
-                nickname = username
-        ))
+        userRepository.save(user)
+        requestBody(put("/users/password"), ChangePasswordRequest(user.email, "4321"))
+    }
 
-        emailVerify()
-        val request = ChangePasswordRequest(testMail, "4321")
-        requestMvc(put("/users/password"), request)
+
+    @Test
+    @Throws
+    fun exitAccountTest() {
+        userRepository.save(user)
+        val request = ChangePasswordRequest(user.email, "1234")
+        val accessToken = "Bearer ${getToken(TokenType.ACCESS, user.email, "1234")}"
+        requestBody(delete("/users"), request, accessToken)
     }
 
 
@@ -79,46 +77,15 @@ class UserApiTest: ApiTest() {
                 password = passwordEncoder.encode(passwd),
                 nickname = username
         ))
-        emailVerify()
-        val request = ChangePasswordRequest(testMail, passwd)
-        requestMvc(delete("/users"), request, "Bearer ${accessToken()}")
     }
 
 
     private fun emailVerify() {
         emailVerificationRepository.save(EmailVerification(
-                email = testMail,
-                authCode = "CODE",
+                email = user.email,
+                authCode = "AUTH_CODE",
                 status = EmailVerificationStatus.UNVERIFID
         ))
-
-        requestMvc(put("/users/email/verify"), VerifyCodeRequest(testMail, "CODE"))
-    }
-
-
-    private fun requestMvc(method: MockHttpServletRequestBuilder, obj: Any? = null, token: String? = ""): String {
-        return mvc.perform(
-                method
-                        .header("Authorization", token)
-                        .content(objectMapper
-                                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-                                .writeValueAsString(obj))
-                        .contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk)
-                .andReturn().response.contentAsString
-    }
-
-
-    private fun accessToken(): String {
-        val content = requestMvc(post("/auth"), SignInRequest(testMail, passwd))
-        val response = mappingResponse(content, TokenResponse::class.java) as TokenResponse
-        return response.accessToken
-    }
-
-
-    private fun mappingResponse(obj: String, cls: Class<*>): Any {
-        return objectMapper
-                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-                .readValue(obj, cls)
+        requestBody(put("/users/email/verify"), VerifyCodeRequest(user.email, "AUTH_CODE"))
     }
 }
