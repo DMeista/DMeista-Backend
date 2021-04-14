d3c6d20a698897984c92a6c7d95b8c9bfbdd58fe
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-04-14 19:33:38 +0900

    [REFACT] - Friend Api TestCode Refactoring

--- src/test/kotlin/sinhee/kang/tutorial/user/FriendApiTest.kt
+++ src/test/kotlin/sinhee/kang/tutorial/user/FriendApiTest.kt
@@ -1,21 +1,14 @@
 package sinhee.kang.tutorial.user
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.MediaType
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import sinhee.kang.tutorial.ApiTest
-import sinhee.kang.tutorial.domain.auth.dto.request.SignInRequest
-import sinhee.kang.tutorial.domain.auth.dto.response.TokenResponse
+import sinhee.kang.tutorial.TokenType
 import sinhee.kang.tutorial.domain.user.domain.friend.enums.FriendStatus
 import sinhee.kang.tutorial.domain.user.domain.friend.repository.FriendRepository
 import sinhee.kang.tutorial.domain.user.domain.user.User
@@ -24,15 +17,9 @@ import sinhee.kang.tutorial.domain.user.dto.response.UserListResponse
 
 class FriendApiTest: ApiTest() {
     @Autowired
-    private lateinit var mvc: MockMvc
-    @Autowired
     private lateinit var userRepository: UserRepository
     @Autowired
     private lateinit var friendRepository: FriendRepository
-    @Autowired
-    private lateinit var objectMapper: ObjectMapper
-
-    private val passwordEncoder: PasswordEncoder = BCryptPasswordEncoder()
 
     private val user1: User = User(
         nickname = "user1",
@@ -53,8 +40,8 @@ class FriendApiTest: ApiTest() {
     fun setup() {
         userRepository.save(user1)
         userRepository.save(user2)
-        user1Token = "Bearer ${accessToken(user1.email)}"
-        user2Token = "Bearer ${accessToken(user2.email)}"
+        user1Token = "Bearer ${getToken(TokenType.ACCESS, user1.email, "1234")}"
+        user2Token = "Bearer ${getToken(TokenType.ACCESS, user2.email, "1234")}"
     }
 
 
@@ -70,7 +57,7 @@ class FriendApiTest: ApiTest() {
     fun getReceiveFriendRequestTest() {
         requestParam(post("/users/friends"), targetName = user2.nickname, token = user1Token)
         val request = mappingResponse(
-            requestContent(get("/users/friends/request"), token = user2Token),
+            requestBody(get("/users/friends/request"), token = user2Token),
             UserListResponse::class.java
         ) as UserListResponse
 
@@ -127,18 +114,6 @@ class FriendApiTest: ApiTest() {
     }
 
 
-    private fun requestContent(method: MockHttpServletRequestBuilder, obj: Any? = null, token: String? = ""): String {
-        return mvc.perform(
-            method
-                .header("Authorization", token)
-                .content(ObjectMapper()
-                    .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-                    .writeValueAsString(obj))
-                .contentType(MediaType.APPLICATION_JSON_VALUE))
-            .andExpect(status().isOk)
-            .andReturn().response.contentAsString
-    }
-
     private fun requestParam(method: MockHttpServletRequestBuilder, targetName: String? = null, token: String? = ""): String {
         return mvc.perform(
             method
@@ -146,18 +121,5 @@ class FriendApiTest: ApiTest() {
                 .param("nickname", targetName))
             .andExpect(status().isOk)
             .andReturn().response.contentAsString
-    }
-
-
-    private fun accessToken(email: String): String {
-        val content = requestContent(post("/auth"), SignInRequest(email, "1234"))
-        val response = mappingResponse(content, TokenResponse::class.java) as TokenResponse
-        return response.accessToken
-    }
-
-    private fun mappingResponse(obj: String, cls: Class<*>): Any {
-        return objectMapper
-                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-                .readValue(obj, cls)
     }
 }
