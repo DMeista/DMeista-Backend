a4184e9f06f4c308983e59a36c8f347099b4ecea
kangsinhee <rkdtlsgml40@dsm.hs.kr>
2021-04-14 19:32:56 +0900

    [REFACT] - Post Api TestCode Refactoring

--- src/test/kotlin/sinhee/kang/tutorial/post/PostApiTest.kt
+++ src/test/kotlin/sinhee/kang/tutorial/post/PostApiTest.kt
@@ -1,139 +1,76 @@
 package sinhee.kang.tutorial.post
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.MediaType
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 import sinhee.kang.tutorial.ApiTest
-import sinhee.kang.tutorial.domain.auth.dto.request.SignInRequest
-import sinhee.kang.tutorial.domain.auth.dto.response.TokenResponse
+import sinhee.kang.tutorial.TokenType
 import sinhee.kang.tutorial.domain.post.domain.post.repository.PostRepository
 import sinhee.kang.tutorial.domain.user.domain.user.User
 import sinhee.kang.tutorial.domain.user.domain.user.repository.UserRepository
 
 class PostApiTest: ApiTest() {
     @Autowired
-    private lateinit var mvc: MockMvc
-    @Autowired
     private lateinit var postRepository: PostRepository
+
     @Autowired
     private lateinit var userRepository: UserRepository
-    @Autowired
-    private lateinit var passwordEncoder: PasswordEncoder
-    @Autowired
-    private lateinit var objectMapper: ObjectMapper
 
-    private val testMail = "rkdtlsgml500@naver.com"
-    private val passwd = "1234"
-    private val username = "user"
+    private val user: User = User(
+        nickname = "user",
+        email = "rkdtlsgml40@dsm.hs.kr",
+        password = passwordEncoder.encode("1234")
+    )
 
+    private lateinit var accessToken: String
 
     @BeforeEach
     fun setup() {
-        userRepository.save(User(
-                email = testMail,
-                password = passwordEncoder.encode(passwd),
-                nickname = username
-        ))
+        userRepository.save(user)
+        accessToken = "Bearer ${getToken(TokenType.ACCESS, user.email, "1234")}"
     }
 
     @AfterEach
     fun clean() {
-        userRepository.findByNickname(username)
-                ?.let { user -> userRepository.delete(user) }
+        postRepository.deleteAll()
+        userRepository.deleteAll()
     }
 
     @Test
     @Throws
     fun uploadPostTest() {
-        val post = uploadOrEditPost(post("/posts"))
+        val post = generatePost(post("/posts"), token = accessToken)
         postRepository.findById(post)
-                .orElseThrow { Exception() }
-                .let { assert(post == it.postId) }
-        postRepository.deleteById(post)
+            .orElseThrow { throw Exception() }
+            .let { assert(post == it.postId) }
     }
 
 
     @Test
     @Throws
     fun editPostTest() {
-        val post = uploadOrEditPost(post("/posts"), "before_title")
-
-        uploadOrEditPost(patch("/posts/$post"), "after_title")
+        val post = generatePost(post("/posts"), token = accessToken)
+        generatePost(patch("/posts/$post"), title = "new title", token = accessToken)
         postRepository.findById(post)
-                .orElseThrow { Exception() }
-                .let { assert(it.title == "after_title") }
-        postRepository.deleteById(post)
+            .orElseThrow { Exception() }
+            .let { assert(it.title == "new title") }
     }
 
 
     @Test
     @Throws
     fun deletePostTest() {
-        val post = uploadOrEditPost(post("/posts"))
-        deletePost(post)
-    }
-
-
-    private fun uploadOrEditPost(method: MockHttpServletRequestBuilder,
-                                 title: String = "title",
-                                 content: String = "content",
-                                 tags: String = "tag, test"
-    ): Int {
-        val accessToken = accessToken()
-        return Integer.parseInt(mvc.perform(
-                method
-                        .header("Authorization", "Bearer $accessToken")
-                        .param("title", title)
-                        .param("content", content)
-                        .param("tags", tags))
-                .andExpect(status().isOk)
-                .andReturn().response.contentAsString)
-    }
-
-
-    fun deletePost(postId: Int) {
-        val accessToken = accessToken()
-        mvc.perform(delete("/posts/$postId")
-                .header("Authorization", "Bearer $accessToken"))
-                .andDo(print())
-                .andExpect(status().isOk)
-                .andReturn().response.contentAsString
-    }
-
-
-    private fun requestMvc(method: MockHttpServletRequestBuilder, obj: Any? = null): String {
-        return mvc.perform(
-                method
-                        .content(ObjectMapper()
-                        .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-                        .writeValueAsString(obj))
-                .contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk)
-                .andReturn().response.contentAsString
-    }
-
-
-    private fun accessToken(): String {
-        val content = requestMvc(post("/auth"), SignInRequest(testMail, passwd))
-        val response = mappingResponse(content, TokenResponse::class.java) as TokenResponse
-        return response.accessToken
-    }
-
-
-    private fun mappingResponse(obj: String, cls: Class<*>): Any {
-        return objectMapper
-                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-                .readValue(obj, cls)
+        val post = generatePost(post("/posts"), token = accessToken)
+        mvc.perform(
+            delete("/posts/$post")
+                .header("Authorization", accessToken)
+        )
+            .andExpect(status().isOk)
+            .andReturn().response.contentAsString
     }
 }
