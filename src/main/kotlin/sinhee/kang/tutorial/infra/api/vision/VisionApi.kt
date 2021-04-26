48492787e7e1f57a50411bb882275bf27f0f83f5
kangsinhee <rkdtlsgml40@naver.com>
2021-04-26 11:47:48 +0900

    [CORRECT] - Change Annotation Level

--- src/main/kotlin/sinhee/kang/tutorial/infra/api/vision/VisionApi.kt
+++ src/main/kotlin/sinhee/kang/tutorial/infra/api/vision/VisionApi.kt
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import sinhee.kang.tutorial.infra.api.vision.dto.VisionResponse
 
-@Service
+@Component
 class VisionApi(
     @Value("\${kakao.rest.api.url}")
     private val url: String,
