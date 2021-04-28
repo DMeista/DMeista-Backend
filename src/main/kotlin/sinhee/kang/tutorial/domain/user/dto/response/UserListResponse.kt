4c1b7c3e601f8474e8ef1c432a6614cf181742fc
kangsinhee <rkdtlsgml40@naver.com>
2021-04-28 09:42:37 +0900

    [MODIFY] - Dto obj type change

--- src/main/kotlin/sinhee/kang/tutorial/domain/user/dto/response/UserListResponse.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/user/dto/response/UserListResponse.kt
@@ -3,5 +3,5 @@ package sinhee.kang.tutorial.domain.user.dto.response
 data class UserListResponse (
     var totalItems: Int,
 
-    var applicationResponses: MutableList<UserResponse>
+    var applicationResponses: List<UserResponse>
 )
