bdb6688af3d3ba15fea225c8a96762d7cd08a1cb
kangsinhee <rkdtlsgml40@naver.com>
2021-05-03 14:43:04 +0900

    WIP

--- src/main/kotlin/sinhee/kang/tutorial/infra/api/vision/dto/ResultResponse.kt
+++ src/main/kotlin/sinhee/kang/tutorial/infra/api/vision/dto/ResultResponse.kt
@@ -1,7 +1,7 @@
 package sinhee.kang.tutorial.infra.api.vision.dto
 
-data class ResultResponse (
-    var label_kr: List<String> = listOf(),
+class ResultResponse {
+    var label_kr: List<String> = listOf()
 
     var label: List<String> = listOf()
-)
+}
