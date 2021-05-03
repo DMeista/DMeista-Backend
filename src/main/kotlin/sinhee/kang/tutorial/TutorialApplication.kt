2217b16251cdeb3d1ffc9bc39cb9369d16e42f2f
kangsinhee <rkdtlsgml40@naver.com>
2021-05-03 22:42:00 +0900

    [REMOVE] - Delete Useless Line

--- src/main/kotlin/sinhee/kang/tutorial/TutorialApplication.kt
+++ src/main/kotlin/sinhee/kang/tutorial/TutorialApplication.kt
@@ -3,7 +3,6 @@ package sinhee.kang.tutorial
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.scheduling.annotation.EnableAsync
 
 @EnableJpaAuditing
 @SpringBootApplication
