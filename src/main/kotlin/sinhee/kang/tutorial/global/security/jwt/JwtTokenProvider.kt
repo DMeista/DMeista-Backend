c685b41b072e8e4bf6beb60d75b33e45c71e88fc
kangsinhee <rkdtlsgml40@naver.com>
2021-05-06 01:21:14 +0900

    [ADD] - Generate Cookie Func 추가 및 코드 정리

--- src/main/kotlin/sinhee/kang/tutorial/global/security/jwt/JwtTokenProvider.kt
+++ src/main/kotlin/sinhee/kang/tutorial/global/security/jwt/JwtTokenProvider.kt
@@ -6,82 +6,71 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
+import sinhee.kang.tutorial.domain.auth.domain.refreshToken.RefreshToken
+import sinhee.kang.tutorial.domain.auth.service.refreshtoken.RefreshTokenService
 import sinhee.kang.tutorial.global.businessException.exception.auth.InvalidTokenException
 import sinhee.kang.tutorial.global.security.auth.AuthDetailsService
 import sinhee.kang.tutorial.global.security.jwt.enums.TokenType
 import java.util.*
-import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletResponse
 
 @Component(value = "jwtTokenProvider")
 class JwtTokenProvider(
-        private val authDetailsService: AuthDetailsService,
+    private val authDetailsService: AuthDetailsService,
 
-        @Value("\${auth.jwt.secret}")
-        private val secretKey: String,
+    private val refreshTokenService: RefreshTokenService,
 
-        @Value("\${auth.jwt.exp.access}")
-        private val accessTokenExpiration: Long,
-
-        @Value("\${auth.jwt.exp.refresh}")
-        private val refreshTokenExpiration: Long,
-
-        @Value("\${auth.jwt.header}")
-        private val header: String,
-
-        @Value("\${auth.jwt.prefix}")
-        private val prefix: String
+    @Value("\${auth.jwt.secret}")
+    private val secretKey: String,
 ) {
-
-    fun generateAccessToken(username: String): String =
-        generateTokenFactory(username, accessTokenExpiration, TokenType.ACCESS)
-
-    fun generateRefreshToken(username: String): String =
-        generateTokenFactory(username, refreshTokenExpiration, TokenType.REFRESH)
-
-    fun resolveToken(request: HttpServletRequest): String? {
-        val bearerToken = request.getHeader(header)
-        return if (bearerToken != null && bearerToken.startsWith(prefix)) {
-            bearerToken.substring(7)
-        } else null
-    }
-
-    fun isValidateToken(token: String): Boolean {
-        try {
-            Jwts.parser().setSigningKey(secretKey)
-                .parseClaimsJws(token).body.subject
-            return true
-        } catch (e: Exception) {
-            throw InvalidTokenException()
-        }
-    }
-
-    fun isRefreshToken(token: String): Boolean =
-        try {
-            Jwts.parser().setSigningKey(secretKey)
-                .parseClaimsJws(token).body["type"] == TokenType.REFRESH
-        } catch (e: Exception) {
-            throw InvalidTokenException()
-        }
 
     fun getAuthentication(token: String): Authentication {
         val authDetails = authDetailsService.loadUserByUsername(getUserNickname(token))
         return UsernamePasswordAuthenticationToken(authDetails, "", authDetails.authorities)
     }
 
-    private fun generateTokenFactory(username: String, tokenExpiration: Long, tokenType: TokenType) =
-        Jwts.builder()
-            .setIssuedAt(Date())
-            .setExpiration(Date(System.currentTimeMillis() + tokenExpiration * 1000))
-            .setSubject(username)
-            .claim("type", tokenType)
-            .signWith(SignatureAlgorithm.HS256, secretKey)
-            .compact()
-
-    private fun getUserNickname(token: String): String =
-        try {
-            Jwts.parser().setSigningKey(secretKey)
-                .parseClaimsJws(token).body.subject
+    fun getUserNickname(token: String): String = try {
+        Jwts.parser().setSigningKey(secretKey)
+            .parseClaimsJws(token).body.subject
         } catch (e: Exception) {
             throw InvalidTokenException()
         }
+
+    fun isValidateToken(token: String): Boolean {
+        return try {
+            Jwts.parser().setSigningKey(secretKey)
+                .parseClaimsJws(token).body.expiration
+                .before(Date())
+        } catch (e: Exception) {
+            throw InvalidTokenException()
+        }
+    }
+
+    fun generateAuthCookies(httpServletResponse: HttpServletResponse, username: String) {
+        val refreshToken = generateTokenFactory(username, TokenType.REFRESH)
+            .apply { refreshTokenService.save(RefreshToken(username, this, TokenType.REFRESH.expiredTokenTime)) }
+        val accessToken = generateTokenFactory(username, TokenType.ACCESS)
+
+        httpServletResponse.apply {
+            addCookie(generateCookieFactory(TokenType.REFRESH, refreshToken))
+            addCookie(generateCookieFactory(TokenType.ACCESS, accessToken))
+        }
+    }
+
+    private fun generateCookieFactory(tokenType: TokenType, token: String): Cookie =
+        Cookie(tokenType.cookieName, token).apply {
+            maxAge = tokenType.expiredTokenTime.toInt()
+            isHttpOnly = true
+            path = "/"
+        }
+
+    private fun generateTokenFactory(username: String, tokenType: TokenType): String =
+        Jwts.builder()
+            .setIssuedAt(Date())
+            .setExpiration(Date(System.currentTimeMillis() + tokenType.expiredTokenTime * 1000))
+            .setSubject(username)
+            .claim("type", tokenType.tokenName)
+            .signWith(SignatureAlgorithm.HS256, secretKey)
+            .compact()
 }
