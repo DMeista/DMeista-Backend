bb852232fd4046f1f5e1aa8c6b785eef8947f579
kangsinhee <rkdtlsgml40@naver.com>
2021-05-06 00:37:03 +0900

    [MODIFY] - Token Enum에 만료시간, 이름 추가

--- src/main/kotlin/sinhee/kang/tutorial/global/security/jwt/enums/TokenType.kt
+++ src/main/kotlin/sinhee/kang/tutorial/global/security/jwt/enums/TokenType.kt
@@ -1,5 +1,6 @@
 package sinhee.kang.tutorial.global.security.jwt.enums
 
-enum class TokenType {
-    ACCESS, REFRESH
+enum class TokenType(var tokenName: String, var cookieName: String, var expiredTokenTime: Long) {
+    ACCESS("access_token", "_Access", 60 * 60 * 2),
+    REFRESH("refresh_token", "_Refresh", 60 * 60 * 24 * 7)
 }
