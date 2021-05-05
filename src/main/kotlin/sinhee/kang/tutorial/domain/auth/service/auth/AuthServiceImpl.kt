85ee87e74e80ee7e5e5d4986a2bf2c96511d4e15
kangsinhee <rkdtlsgml40@naver.com>
2021-05-06 00:38:29 +0900

    [DELETE] - Refresh-Token API 삭제

--- src/main/kotlin/sinhee/kang/tutorial/domain/auth/service/auth/AuthServiceImpl.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/auth/service/auth/AuthServiceImpl.kt
@@ -1,68 +1,30 @@
 package sinhee.kang.tutorial.domain.auth.service.auth
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
-import sinhee.kang.tutorial.domain.auth.domain.refreshToken.RefreshToken
-import sinhee.kang.tutorial.domain.auth.domain.refreshToken.repository.RefreshTokenRepository
 import sinhee.kang.tutorial.domain.auth.dto.request.SignInRequest
-import sinhee.kang.tutorial.domain.auth.dto.response.TokenResponse
-import sinhee.kang.tutorial.global.businessException.exception.auth.ExpiredTokenException
-import sinhee.kang.tutorial.global.businessException.exception.auth.InvalidTokenException
-import sinhee.kang.tutorial.domain.auth.service.refreshtoken.RefreshTokenService
 import sinhee.kang.tutorial.global.businessException.exception.auth.UnAuthorizedException
 import sinhee.kang.tutorial.domain.user.domain.user.User
 import sinhee.kang.tutorial.domain.user.domain.user.repository.UserRepository
 import sinhee.kang.tutorial.global.security.auth.AuthenticationFacade
 import sinhee.kang.tutorial.global.security.jwt.JwtTokenProvider
 import sinhee.kang.tutorial.global.businessException.exception.common.UserNotFoundException
+import javax.servlet.http.HttpServletResponse
 
 @Service
 class AuthServiceImpl(
-        private val passwordEncoder: PasswordEncoder,
-        private val authenticationFacade: AuthenticationFacade,
-
-        private val userRepository: UserRepository,
-        private val refreshTokenRepository: RefreshTokenRepository,
-
-        private val refreshTokenService: RefreshTokenService,
-        private val tokenProvider: JwtTokenProvider,
-
-        @Value("\${auth.jwt.exp.refresh}")
-        private val refreshExp: Long,
-
-        @Value("\${auth.jwt.prefix}")
-        private val tokenType: String
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder,
+    private val tokenProvider: JwtTokenProvider,
+    private val authenticationFacade: AuthenticationFacade,
 ) : AuthService {
 
-    override fun signIn(request: SignInRequest): TokenResponse {
-        return userRepository.findByEmail(request.email)
-                ?.takeIf { user -> passwordEncoder.matches(request.password, user.password) }
-                ?.let { user ->
-                    val accessToken: String = tokenProvider.generateAccessToken(user.nickname)
-                    val refreshToken: String = tokenProvider.generateRefreshToken(user.nickname)
-                    refreshTokenService.save(RefreshToken(user.nickname, refreshToken, refreshExp))
-                    TokenResponse(accessToken, refreshToken, tokenType)
-                }
-                ?: run { throw UserNotFoundException() }
-    }
-
-    override fun refreshToken(refreshToken: String): TokenResponse {
-        if (!tokenProvider.isRefreshToken(refreshToken)) {
-            throw InvalidTokenException()
-        }
-
-        return refreshTokenRepository.findByRefreshToken(refreshToken)
-                ?.let { receivedToken ->
-                    val generatedRefreshToken = tokenProvider.generateRefreshToken(receivedToken.nickname)
-                    val generatedAccessToken = tokenProvider.generateAccessToken(receivedToken.nickname)
-                    receivedToken.update(generatedRefreshToken, refreshExp)
-                    refreshTokenRepository.save(receivedToken)
-
-                    TokenResponse(generatedAccessToken, generatedRefreshToken, tokenType)
-                }
-                ?: throw ExpiredTokenException()
+    override fun signIn(request: SignInRequest, response: HttpServletResponse) {
+        val user = userRepository.findByEmail(request.email)
+            ?: run { throw UserNotFoundException() }
+        if (passwordEncoder.matches(request.password, user.password))
+            tokenProvider.addCookies(response, user.nickname)
     }
 
     override fun authValidate(): User {
