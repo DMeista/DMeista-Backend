aa1ed941eb7ab72919af19b8b37b13de6617eb20
kangsinhee <rkdtlsgml40@naver.com>
2021-05-06 15:31:01 +0900

    [REMOVE] - 의미 없는 코드 삭제

--- src/main/kotlin/sinhee/kang/tutorial/global/security/jwt/JwtConfigurer.kt
+++ src/main/kotlin/sinhee/kang/tutorial/global/security/jwt/JwtConfigurer.kt
@@ -1,14 +1,12 @@
 package sinhee.kang.tutorial.global.security.jwt
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 class JwtConfigurer(
-    @Autowired
-    private val jwtTokenProvider: JwtTokenProvider
+    private val jwtTokenProvider: JwtTokenProvider,
 ) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
 
     override fun configure(httpSecurity: HttpSecurity) {
