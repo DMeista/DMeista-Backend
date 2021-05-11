57a57b4e5e60ddab4c07650c6c63e94e47465685
kangsinhee <rkdtlsgml40@naver.com>
2021-05-11 16:40:35 +0900

    [MODIFY] - Post Package 내 코드 정리

--- src/main/kotlin/sinhee/kang/tutorial/domain/post/service/emoji/EmojiService.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/post/service/emoji/EmojiService.kt
@@ -6,5 +6,6 @@ import sinhee.kang.tutorial.domain.post.dto.response.PostEmojiListResponse
 
 interface EmojiService {
     fun getPostEmojiUserList(postId: Int): PostEmojiListResponse
+
     fun emojiService(postId: Int, status: EmojiStatus): EmojiResponse?
 }
