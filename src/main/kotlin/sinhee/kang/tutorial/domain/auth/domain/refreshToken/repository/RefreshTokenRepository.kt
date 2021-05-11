89380831c75e708272da5061e211201ffcecb660
kangsinhee <rkdtlsgml40@naver.com>
2021-05-11 16:04:30 +0900

    [REMOVE] - RefreshTokenRepository 사용하지 않는 쿼리 메소드  삭제

--- src/main/kotlin/sinhee/kang/tutorial/domain/auth/domain/refreshToken/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/auth/domain/refreshToken/repository/RefreshTokenRepository.kt
@@ -5,6 +5,4 @@ import org.springframework.stereotype.Repository
 import sinhee.kang.tutorial.domain.auth.domain.refreshToken.RefreshToken
 
 @Repository
-interface RefreshTokenRepository : CrudRepository<RefreshToken, Int> {
-    fun findByRefreshToken(refreshToken: String): RefreshToken?
-}
+interface RefreshTokenRepository : CrudRepository<RefreshToken, Int>
