324e2558ffdca74d9421c6853748647784fc5e04
kangsinhee <rkdtlsgml40@naver.com>
2021-05-11 10:53:38 +0900

    [REMOVE] - Remove Useless Service

--- src/main/kotlin/sinhee/kang/tutorial/domain/auth/service/refreshtoken/RefreshTokenService.kt
+++
@@ -1,7 +0,0 @@
-package sinhee.kang.tutorial.domain.auth.service.refreshtoken
-
-import sinhee.kang.tutorial.domain.auth.domain.refreshToken.RefreshToken
-
-interface RefreshTokenService {
-    fun save(refreshToken: RefreshToken)
-}
