1f0f40b69104dbf1485be8594626a2d978cc09ef
kangsinhee <rkdtlsgml40@naver.com>
2021-05-11 16:53:49 +0900

    [MODIFY] - View Entity  val로 변수 변경

--- src/main/kotlin/sinhee/kang/tutorial/domain/post/domain/view/View.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/post/domain/view/View.kt
@@ -7,17 +7,17 @@ import javax.persistence.*
 
 @Entity(name = "tbl_view")
 data class View (
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var Id: Int = 0,
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val Id: Int = 0,
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user", nullable = true)
-    var user: User,
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "user", nullable = true)
+        val user: User,
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post", nullable = true)
-    var post: Post,
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "post", nullable = true)
+        val post: Post,
 
-    @Column(nullable = false)
-    var createdAt: LocalDateTime = LocalDateTime.now()
+        @Column(nullable = false)
+        val createdAt: LocalDateTime = LocalDateTime.now()
 )
