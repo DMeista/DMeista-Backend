43fe014287707f6896f8ea66327a24355d0589bd
kangsinhee <rkdtlsgml40@naver.com>
2021-05-28 13:53:14 +0900

    [REMOVE] - Spring Validation in dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,6 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-cache")
 
     implementation("org.springframework.boot:spring-boot-starter-security")
