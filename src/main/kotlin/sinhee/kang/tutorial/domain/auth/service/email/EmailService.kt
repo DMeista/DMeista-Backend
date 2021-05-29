dd44a0718ec12be4784312225af384f91d8f9701
kangsinhee <rkdtlsgml40@naver.com>
2021-05-30 05:57:56 +0900

    [MODIFY] - Rename & Delete Validate functions in EmailService

--- src/main/kotlin/sinhee/kang/tutorial/domain/auth/service/email/EmailService.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/auth/service/email/EmailService.kt
@@ -5,15 +5,9 @@ import sinhee.kang.tutorial.domain.auth.dto.request.VerifyCodeRequest
 import sinhee.kang.tutorial.domain.user.domain.user.User
 
 interface EmailService {
-    fun sendVerificationEmail(emailRequest: EmailRequest, sendType: SendType)
+    fun sendVerifyEmail(emailRequest: EmailRequest, sendType: SendType)
 
-    fun setVerifyEmail(verifyCodeRequest: VerifyCodeRequest)
+    fun verifyAuthCode(verifyCodeRequest: VerifyCodeRequest)
 
     fun sendCelebrateEmail(user: User)
-
-    fun String.isVerifyEmail(): String
-
-    fun String.isValidationEmail(): String
-
-    fun String.isExistEmail(sendType: SendType?): String
 }
