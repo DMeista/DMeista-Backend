5d4fe8e9ad3129884d237c68e40eca1cbac4175e
kangsinhee <rkdtlsgml40@naver.com>
2021-06-01 10:48:46 +0900

    [ADD] - Generate AccessDenied BusinessException

--- src/main/kotlin/sinhee/kang/tutorial/global/businessException/exception/auth/PermissionDeniedException.kt
+++ src/main/kotlin/sinhee/kang/tutorial/global/businessException/exception/auth/PermissionDeniedException.kt
@@ -3,5 +3,5 @@ package sinhee.kang.tutorial.global.businessException.exception.auth
 import sinhee.kang.tutorial.global.businessException.BusinessException
 import sinhee.kang.tutorial.global.businessException.ErrorCode
 
-class PermissionDeniedException : BusinessException(ErrorCode.PERMISSION_DENIED_EXCEPTION) {
+class PermissionDeniedException : BusinessException(ErrorCode.PERMISSION_DENIED) {
 }
