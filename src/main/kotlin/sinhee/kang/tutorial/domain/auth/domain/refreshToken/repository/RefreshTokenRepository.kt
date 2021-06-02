dc2f39e0e7d0a58798d9cdcd4b78867279d00d0b
kangsinhee <rkdtlsgml40@naver.com>
2021-06-02 22:39:45 +0900

    [REMOVE] - RefreshToken Model 삭제

--- src/main/kotlin/sinhee/kang/tutorial/domain/auth/domain/refreshToken/repository/RefreshTokenRepository.kt
+++
@@ -1,8 +0,0 @@
-package sinhee.kang.tutorial.domain.auth.domain.refreshToken.repository
-
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-import sinhee.kang.tutorial.domain.auth.domain.refreshToken.RefreshToken
-
-@Repository
-interface RefreshTokenRepository : CrudRepository<RefreshToken, Int>
