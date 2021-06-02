b41f02cae1fa652b1eb5bef6d6dae2614c5c0734
kangsinhee <rkdtlsgml40@naver.com>
2021-06-02 23:28:58 +0900

    [ADD] - Generate DetectAdultImage func

--- src/main/kotlin/sinhee/kang/tutorial/infra/api/kakao/KakaoApi.kt
+++ src/main/kotlin/sinhee/kang/tutorial/infra/api/kakao/KakaoApi.kt
@@ -3,6 +3,7 @@ package sinhee.kang.tutorial.infra.api.kakao
 import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.http.*
+import sinhee.kang.tutorial.infra.api.kakao.adult.dto.AdultResponse
 import sinhee.kang.tutorial.infra.api.kakao.vision.dto.VisionResponse
 
 interface KakaoApi {
@@ -12,4 +13,11 @@ interface KakaoApi {
         @Header("Authorization") token: String,
         @Part imageFile: MultipartBody.Part
     ): Call<VisionResponse>
+
+    @Multipart
+    @POST("/adult/detect")
+    fun detectAdultImage(
+        @Header("Authorization") token: String,
+        @Part imageFile: MultipartBody.Part
+    ): Call<AdultResponse>
 }
