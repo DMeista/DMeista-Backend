0ccdb27afe8258775ec61db78b1e01752899f8cb
kangsinhee <rkdtlsgml40@naver.com>
2021-06-03 14:56:07 +0900

    [RENAME] - TestLib 이름 변경

--- src/test/kotlin/sinhee/kang/tutorial/TestLib.kt
+++ src/test/kotlin/sinhee/kang/tutorial/TestLib.kt
@@ -5,20 +5,48 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import sinhee.kang.tutorial.domain.auth.domain.verification.SignUpVerification
+import sinhee.kang.tutorial.domain.auth.domain.verification.enums.EmailVerificationStatus
 
 import sinhee.kang.tutorial.domain.auth.dto.request.SignInRequest
 import sinhee.kang.tutorial.domain.auth.dto.response.TokenResponse
+import sinhee.kang.tutorial.domain.user.domain.user.User
 import sinhee.kang.tutorial.infra.redis.EmbeddedRedisConfig
+import javax.servlet.http.Cookie
 
 @SpringBootTest(classes = [TutorialApplication::class, EmbeddedRedisConfig::class],
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test", "local")
-class TestApis: DiVariables() {
+class TestLib: DiVariables() {
 
-    protected fun requestBody(method: MockHttpServletRequestBuilder, obj: Any): String {
+    protected fun requestBody(method: MockHttpServletRequestBuilder, obj: Any): ResultActions =
+        mvc.perform(method
+            .content(objectMapper
+                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                .writeValueAsString(obj))
+            .contentType(MediaType.APPLICATION_JSON_VALUE))
+
+    protected fun requestBody(method: MockHttpServletRequestBuilder, obj: Any? = null, cookie: Cookie?): ResultActions =
+        mvc.perform(method
+            .cookie(cookie)
+            .content(objectMapper
+                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                .writeValueAsString(obj))
+            .contentType(MediaType.APPLICATION_JSON_VALUE))
+
+    protected fun requestBody(method: MockHttpServletRequestBuilder, obj: Any? = null, token: String?): ResultActions =
+        mvc.perform(method
+                .header("Authorization", token)
+                .content(ObjectMapper()
+                    .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                    .writeValueAsString(obj))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+
+    protected fun requestBody1(method: MockHttpServletRequestBuilder, obj: Any): String {
         return mvc.perform(
             method
                 .content(ObjectMapper()
@@ -29,7 +57,7 @@ class TestApis: DiVariables() {
             .andReturn().response.contentAsString
     }
 
-    protected fun requestBody(method: MockHttpServletRequestBuilder, obj: Any? = null, token: String?): String {
+    protected fun requestBody1(method: MockHttpServletRequestBuilder, obj: Any? = null, token: String?): String {
         return mvc.perform(
             method
                 .header("Authorization", token)
@@ -43,9 +71,26 @@ class TestApis: DiVariables() {
 
     protected fun getAccessToken(signInRequest: SignInRequest): String {
         val content = requestBody(post("/auth"), signInRequest)
-        val response = mappingResponse(content, TokenResponse::class.java) as TokenResponse
+            .andReturn().response.contentAsString
+        val tokenResponse = mappingResponse(content, TokenResponse::class.java) as TokenResponse
 
-        return "Bearer " + response.accessToken
+        return "Bearer ${tokenResponse.accessToken}"
+    }
+
+    protected fun getRefreshToken(signInRequest: SignInRequest): Cookie? {
+        return requestBody(post("/auth"), signInRequest)
+            .andReturn().response.cookies.first { it.name == "_Refresh" }
+    }
+
+    protected fun emailVerify(user: User) {
+        signUpVerificationRepository.save(
+            SignUpVerification(
+                email = user.email,
+                authCode = "AUTH-CODE",
+                emailStatus = EmailVerificationStatus.VERIFIED,
+                nickname = user.nickname
+            )
+        )
     }
 
     protected fun generatePost(
