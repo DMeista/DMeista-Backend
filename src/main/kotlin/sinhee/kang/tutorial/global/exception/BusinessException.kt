e06645259f88809459da453238db5e4c7cc6a0eb
kangsinhee <rkdtlsgml40@naver.com>
2021-06-14 12:04:34 +0900

    [ADD] - Generate BusinessException

---
+++ src/main/kotlin/sinhee/kang/tutorial/global/exception/BusinessException.kt
@@ -0,0 +1,5 @@
+package sinhee.kang.tutorial.global.exception
+
+import org.springframework.http.HttpStatus
+
+open class BusinessException(var status: HttpStatus, override var message: String?): RuntimeException()
