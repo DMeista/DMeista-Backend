a69efee833a081f4904784c14c37b40655d096d0
kangsinhee <rkdtlsgml40@naver.com>
2021-06-16 22:19:26 +0900

    [FIX] - Variable initialization Error

--- src/main/kotlin/sinhee/kang/tutorial/global/exception/exceptions/badRequest/BadRequestException.kt
+++ src/main/kotlin/sinhee/kang/tutorial/global/exception/exceptions/badRequest/BadRequestException.kt
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus.BAD_REQUEST
 import sinhee.kang.tutorial.global.exception.BusinessException
 
 open class BadRequestException: BusinessException {
-    constructor(): super(BAD_REQUEST)
-    constructor(message: String): super(BAD_REQUEST, message = message)
+    constructor(): super(BAD_REQUEST, BAD_REQUEST.reasonPhrase)
+    constructor(message: String): super(BAD_REQUEST, message)
 }
--- src/main/kotlin/sinhee/kang/tutorial/global/exception/exceptions/conflict/ConflictException.kt
+++ src/main/kotlin/sinhee/kang/tutorial/global/exception/exceptions/conflict/ConflictException.kt
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus.CONFLICT
 import sinhee.kang.tutorial.global.exception.BusinessException
 
 open class ConflictException: BusinessException {
-    constructor(): super(CONFLICT)
+    constructor(): super(CONFLICT, CONFLICT.reasonPhrase)
     constructor(message: String): super(CONFLICT, message)
 }
