a3f3fb2f8a0cb55f19f7c6b9d3c8a617de453973
kangsinhee <rkdtlsgml40@naver.com>
2021-10-18 03:51:35 +0900

    [MODIFY] - ExceptionResponse class -> data class로 변경

---
+++ src/main/kotlin/sinhee/kang/tutorial/global/exception/dto/ExceptionResponse.kt
@@ -0,0 +1,7 @@
+package sinhee.kang.tutorial.global.exception.dto
+
+data class ExceptionResponse(
+    var status: Int,
+
+    var message: String?
+)
--- src/main/kotlin/sinhee/kang/tutorial/global/exception/dto/response/ExceptionResponse.kt
+++
@@ -1,6 +0,0 @@
-package sinhee.kang.tutorial.global.exception.dto.response
-
-class ExceptionResponse(
-    var status: Int,
-    var message: String?
-)
