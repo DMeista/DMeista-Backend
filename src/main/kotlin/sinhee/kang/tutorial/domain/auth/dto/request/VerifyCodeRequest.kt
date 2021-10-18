eb4bfdb1071d4fb81caf83b7203a2676b9494b5e
kangsinhee <rkdtlsgml40@naver.com>
2021-10-18 14:48:15 +0900

    [REMOVE] - Verify~Request Dto 삭제

--- src/main/kotlin/sinhee/kang/tutorial/domain/auth/dto/request/VerifyCodeRequest.kt
+++
@@ -1,7 +0,0 @@
-package sinhee.kang.tutorial.domain.auth.dto.request
-
-data class VerifyCodeRequest(
-    val email: String,
-
-    val authCode: String
-)
--- src/main/kotlin/sinhee/kang/tutorial/domain/auth/dto/request/VerifyNicknameRequest.kt
+++
@@ -1,7 +0,0 @@
-package sinhee.kang.tutorial.domain.auth.dto.request
-
-data class VerifyNicknameRequest(
-    val email: String,
-
-    val nickname: String
-)
