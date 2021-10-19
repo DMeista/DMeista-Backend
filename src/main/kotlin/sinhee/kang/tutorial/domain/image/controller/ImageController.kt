e6b79cf12bb9e8fce880ccaef04841c6c0ad8625
kangsinhee <rkdtlsgml40@naver.com>
2021-10-19 19:33:07 +0900

    [REFACTOR] - ImageServiceImpl 리팩토링
    코드 정리, Image 유효성 확인 코드 추가

--- src/main/kotlin/sinhee/kang/tutorial/domain/image/controller/ImageController.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/image/controller/ImageController.kt
@@ -1,20 +1,22 @@
-package sinhee.kang.tutorial.domain.file.controller
+package sinhee.kang.tutorial.domain.image.controller
 
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import sinhee.kang.tutorial.domain.file.service.ImageService
+import sinhee.kang.tutorial.domain.image.service.ImageService
 
 @RestController
 @RequestMapping("/image")
 class ImageController(
     private val imageService: ImageService
 ) {
-    @GetMapping(value = ["/{imageName}"],
-                produces = [MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_GIF_VALUE])
+    @GetMapping(
+        value = ["/{imageName}"],
+        produces = [MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_GIF_VALUE]
+    )
     fun getImages(@PathVariable imageName: String): ByteArray {
-        return imageService.getImage(imageName)
+        return imageService.getImageFile(imageName)
     }
 }
---
+++ src/main/kotlin/sinhee/kang/tutorial/domain/image/service/ImageServiceImpl.kt
@@ -0,0 +1,71 @@
+package sinhee.kang.tutorial.domain.image.service
+
+import org.apache.commons.io.IOUtils
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import sinhee.kang.tutorial.domain.image.entity.ImageFile
+import sinhee.kang.tutorial.domain.image.repository.ImageFileRepository
+import sinhee.kang.tutorial.global.exception.exceptions.notFound.ImageNotFoundException
+import sinhee.kang.tutorial.domain.post.entity.post.Post
+
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
+import java.nio.file.Files
+import java.util.*
+import javax.imageio.ImageIO
+
+@Service
+class ImageServiceImpl(
+    @Value("\${upload.path}")
+    private val uploadPath: String,
+
+    private val imageFileRepository: ImageFileRepository
+): ImageService {
+
+    @Cacheable(value = ["cache"])
+    override fun getImageFile(imageName: String): ByteArray {
+        val file = File(uploadPath, imageName)
+
+        if (!file.exists() || !file.isValidImage())
+            throw ImageNotFoundException()
+
+        return IOUtils.toByteArray(FileInputStream(file))
+    }
+
+    override fun saveImageFiles(post: Post, imageFiles: Array<MultipartFile>) {
+        for (image in imageFiles) {
+            val fileName: String = generateUniqueUUID()
+            image.transferTo(File(uploadPath, fileName))
+
+            imageFileRepository.save(
+                ImageFile(post = post, fileName = fileName)
+            )
+        }
+    }
+
+    override fun removeImageFiles(post: Post, imageFiles: List<ImageFile>) {
+        for (image in imageFiles) {
+            Files.delete(File(uploadPath, image.fileName).toPath())
+        }
+        imageFileRepository.deleteByPost(post)
+    }
+
+    private fun generateUniqueUUID(): String {
+        var fileName: String
+        do {
+            fileName = UUID.randomUUID().toString()
+        } while (fileName.isExistImage())
+
+        return fileName
+    }
+
+    private fun String.isExistImage(): Boolean =
+        File(uploadPath, this).exists()
+
+    private fun File.isValidImage(): Boolean =
+        try { ImageIO.read(this) != null }
+        catch (e: IOException) { false }
+}
