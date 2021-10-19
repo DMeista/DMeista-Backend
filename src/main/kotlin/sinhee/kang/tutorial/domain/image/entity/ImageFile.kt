2fc1f7e4edbca0810ddc0e5be6e812e8896657bf
kangsinhee <rkdtlsgml40@naver.com>
2021-10-19 19:26:59 +0900

    [RENAME] - image domain package 이름 변경

---
+++ src/main/kotlin/sinhee/kang/tutorial/domain/image/entity/ImageFile.kt
@@ -0,0 +1,16 @@
+package sinhee.kang.tutorial.domain.image.entity
+
+import sinhee.kang.tutorial.domain.post.entity.post.Post
+import javax.persistence.*
+
+@Entity(name = "tbl_image")
+class ImageFile (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "image")
+    val post: Post,
+
+    val fileName: String
+)
--- src/main/kotlin/sinhee/kang/tutorial/domain/image/repository/ImageFileRepository.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/image/repository/ImageFileRepository.kt
@@ -1,9 +1,9 @@
-package sinhee.kang.tutorial.domain.file.domain.repository
+package sinhee.kang.tutorial.domain.image.repository
 
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import sinhee.kang.tutorial.domain.file.domain.ImageFile
-import sinhee.kang.tutorial.domain.post.domain.post.Post
+import sinhee.kang.tutorial.domain.image.entity.ImageFile
+import sinhee.kang.tutorial.domain.post.entity.post.Post
 
 @Repository
 interface ImageFileRepository: CrudRepository<ImageFile, Int> {
