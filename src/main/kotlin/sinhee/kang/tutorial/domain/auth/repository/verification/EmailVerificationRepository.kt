490af9df236eff177cc195aa0924807b5b48514d
kangsinhee <rkdtlsgml40@naver.com>
2021-10-19 13:01:38 +0900

    [ADD] - findByEmail 메서드 추가

---
+++ src/main/kotlin/sinhee/kang/tutorial/domain/auth/repository/verification/EmailVerificationRepository.kt
@@ -0,0 +1,11 @@
+package sinhee.kang.tutorial.domain.auth.repository.verification
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import sinhee.kang.tutorial.domain.auth.entity.verification.EmailVerification
+import java.util.*
+
+@Repository
+interface EmailVerificationRepository: CrudRepository<EmailVerification, UUID> {
+    fun findByEmail(email: String): EmailVerification?
+}
