451249baea5aa0df922a88e2dbb47711af6cfe6a
kangsinhee <rkdtlsgml40@naver.com>
2021-10-19 13:00:18 +0900

    [MODIFY] - EmailRequestLimiter 하위 Package 변경

--- src/main/kotlin/sinhee/kang/tutorial/domain/auth/repository/requestLimiter/EmailRequestLimiterRepository.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/auth/repository/requestLimiter/EmailRequestLimiterRepository.kt
@@ -1,8 +1,8 @@
-package sinhee.kang.tutorial.domain.auth.repository.emailLimiter
+package sinhee.kang.tutorial.domain.auth.repository.requestLimiter
 
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import sinhee.kang.tutorial.domain.auth.entity.emailLimiter.EmailRequestLimiter
+import sinhee.kang.tutorial.domain.auth.entity.requestLimiter.EmailRequestLimiter
 
 @Repository
 interface EmailRequestLimiterRepository : CrudRepository<EmailRequestLimiter, String>
