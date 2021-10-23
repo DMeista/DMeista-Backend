7d864e602a69397bcdbaccde18a9759294b7eb5e
kangsinhee <rkdtlsgml40@naver.com>
2021-10-23 23:06:24 +0900

    [ADD] - EmojiResponse 생성자 추가 및 변수 순서 변경

--- src/main/kotlin/sinhee/kang/tutorial/domain/post/dto/response/EmojiResponse.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/post/dto/response/EmojiResponse.kt
@@ -1,11 +1,19 @@
 package sinhee.kang.tutorial.domain.post.dto.response
 
+import sinhee.kang.tutorial.domain.post.entity.emoji.Emoji
 import sinhee.kang.tutorial.domain.post.entity.emoji.enums.EmojiStatus
+import sinhee.kang.tutorial.domain.user.entity.user.User
 
 data class EmojiResponse(
-    val username: String? = null,
-
     val postId: Int? = 0,
 
+    val username: String? = null,
+
     val emojiStatus: EmojiStatus? = null
-)
+) {
+    constructor(emoji: Emoji): this(
+        postId = emoji.post.postId,
+        username = emoji.user.nickname,
+        emojiStatus = emoji.status
+    )
+}
