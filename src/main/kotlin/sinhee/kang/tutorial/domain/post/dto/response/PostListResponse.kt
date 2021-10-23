04d94fe742ecc7736bbe836e492c6106b23bf964
kangsinhee <rkdtlsgml40@naver.com>
2021-10-23 20:52:08 +0900

    [ADD] - PostListResponse 생성자 추가 및 applicationResponses 네이밍 변경

--- src/main/kotlin/sinhee/kang/tutorial/domain/post/dto/response/PostListResponse.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/post/dto/response/PostListResponse.kt
@@ -1,9 +1,18 @@
 package sinhee.kang.tutorial.domain.post.dto.response
 
+import org.springframework.data.domain.Page
+import sinhee.kang.tutorial.domain.post.entity.post.Post
+
 data class PostListResponse (
     val totalItems: Int = 0,
 
     val totalPages: Int = 0,
 
-    val applicationResponses: MutableList<PostResponse> = arrayListOf()
-)
+    val applications: MutableList<PostResponse> = mutableListOf()
+) {
+    constructor(posts: Page<Post>, applications: MutableList<PostResponse>): this (
+        totalItems = posts.totalElements.toInt(),
+        totalPages = posts.totalPages,
+        applications = applications
+    )
+}
