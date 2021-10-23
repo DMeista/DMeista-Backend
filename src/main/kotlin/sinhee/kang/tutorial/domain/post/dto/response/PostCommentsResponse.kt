f0f0ec8a159eb22d6ee2e69a18d8e75ba73668f8
kangsinhee <rkdtlsgml40@naver.com>
2021-10-23 20:47:14 +0900

    [ADD] - Comment Response 생성자 및 SubCommentResponse func 추가

--- src/main/kotlin/sinhee/kang/tutorial/domain/post/dto/response/PostCommentsResponse.kt
+++ src/main/kotlin/sinhee/kang/tutorial/domain/post/dto/response/PostCommentsResponse.kt
@@ -1,7 +1,8 @@
 package sinhee.kang.tutorial.domain.post.dto.response
 
 import com.fasterxml.jackson.annotation.JsonFormat
-import sinhee.kang.tutorial.domain.user.domain.user.enums.AccountRole
+import sinhee.kang.tutorial.domain.post.entity.comment.Comment
+import sinhee.kang.tutorial.domain.user.entity.user.User
 import java.time.LocalDateTime
 
 data class PostCommentsResponse(
@@ -17,4 +18,22 @@ data class PostCommentsResponse(
     val isMine: Boolean,
 
     val subComments: MutableList<PostSubCommentsResponse>
-)
+) {
+    constructor(user: User?, comment: Comment): this(
+        commentId = comment.commentId,
+        content = comment.content,
+        createdAt = comment.createdAt,
+        author = comment.user.nickname,
+        isMine = (comment.user == user),
+        subComments = getCommentSubComment(user, comment)
+    )
+
+    companion object {
+        private fun getCommentSubComment(user: User?, comment: Comment): MutableList<PostSubCommentsResponse> =
+            mutableListOf<PostSubCommentsResponse>().apply {
+                comment.subCommentList.forEach { subComment ->
+                    add(PostSubCommentsResponse(user, subComment))
+                }
+            }
+    }
+}
