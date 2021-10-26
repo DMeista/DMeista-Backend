200344be8dfd9901db31b09bd0b8093f3a37fe50
kangsinhee <rkdtlsgml40@naver.com>
2021-10-26 10:30:11 +0900

    [MODIFY] - Friend Entity Package 이동 및 Useless method 삭제

---
+++ src/main/kotlin/sinhee/kang/tutorial/domain/user/entity/friend/Friend.kt
@@ -0,0 +1,45 @@
+package sinhee.kang.tutorial.domain.user.entity.friend
+
+import sinhee.kang.tutorial.domain.user.entity.user.User
+import sinhee.kang.tutorial.domain.user.domain.friend.enums.FriendStatus
+import sinhee.kang.tutorial.domain.user.dto.response.UserResponse
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity(name = "tbl_friend")
+class Friend(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int = 0,
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    var status: FriendStatus = FriendStatus.REQUEST,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user")
+    val user: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "targetUser")
+    val targetUser: User,
+
+    @Column(nullable = false)
+    val connectedAt: LocalDateTime = LocalDateTime.now()
+
+) {
+    constructor(user: User, targetUser: User): this(
+        user = user,
+        targetUser = targetUser,
+        connectedAt = LocalDateTime.now()
+    )
+
+    fun updateStatus(targetId: User): Friend =
+        apply { status = FriendStatus.ACCEPT }
+
+    fun isStatus(friendStatus: FriendStatus): Boolean =
+        status == friendStatus
+
+    fun toUserResponse(user: User): UserResponse =
+        UserResponse(this, user)
+}
