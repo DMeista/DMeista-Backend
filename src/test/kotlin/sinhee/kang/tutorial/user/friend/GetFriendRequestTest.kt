ef02f61b2afa63e3777ca48989e99a81a3250862
kangsinhee <rkdtlsgml40@naver.com>
2021-10-27 15:56:53 +0900

    [REFACTOR] - UserController APIs TestCode

--- src/test/kotlin/sinhee/kang/tutorial/user/friend/GetFriendRequestTest.kt
+++ src/test/kotlin/sinhee/kang/tutorial/user/friend/GetFriendRequestTest.kt
@@ -1,17 +1,19 @@
-package sinhee.kang.tutorial.friend
+package sinhee.kang.tutorial.user.friend
 
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import sinhee.kang.tutorial.TestFactory
 
-import sinhee.kang.tutorial.TestLib
-import sinhee.kang.tutorial.domain.user.domain.friend.Friend
+import sinhee.kang.tutorial.domain.user.entity.friend.Friend
 import sinhee.kang.tutorial.domain.user.dto.response.UserListResponse
 
 @Suppress("NonAsciiCharacters")
-class ReceiveFriendRequestTest: TestLib() {
+class GetFriendRequestTest: TestFactory() {
+
+    private val testPath = "/users/friends/request"
 
     @BeforeEach
     fun setup() {
@@ -30,23 +32,21 @@ class ReceiveFriendRequestTest: TestLib() {
     }
 
     @Test
-    fun `받은 친구요청`() {
-        friendRepository.save(Friend(
-            user = user2,
-            targetUser = user
-        ))
-        val response = requestBody(get("/users/friends/request"), token = currentUserToken)
+    fun `받은 친구요청 - Ok`() {
+        friendRepository.save(Friend(user2, user))
+
+        val response = requestBody(get(testPath), token = currentUserToken)
             .andExpect(MockMvcResultMatchers.status().isOk)
             .andReturn().response.contentAsString
 
         val userListResponse = mappingResponse(response, UserListResponse::class.java) as UserListResponse
 
-        assert(userListResponse.applicationResponses[0].nickname == user2.nickname)
+        assert(userListResponse.applications[0].nickname == user2.nickname)
     }
 
     @Test
-    fun `사용자 인증이 확인되지 않음`() {
-        requestBody(get("/users/friends/request"))
+    fun `사용자 인증이 확인되지 않음 - Unauthorized`() {
+        requestBody(get(testPath))
             .andExpect(MockMvcResultMatchers.status().isUnauthorized)
     }
 }
--- src/test/kotlin/sinhee/kang/tutorial/user/friend/SendFriendRequestTest.kt
+++ src/test/kotlin/sinhee/kang/tutorial/user/friend/SendFriendRequestTest.kt
@@ -1,4 +1,4 @@
-package sinhee.kang.tutorial.friend
+package sinhee.kang.tutorial.user.friend
 
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
@@ -7,13 +7,15 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
+import sinhee.kang.tutorial.TestFactory
 
-import sinhee.kang.tutorial.TestLib
-import sinhee.kang.tutorial.domain.user.domain.friend.Friend
+import sinhee.kang.tutorial.domain.user.entity.friend.Friend
 import sinhee.kang.tutorial.domain.user.domain.friend.enums.FriendStatus
 
 @Suppress("NonAsciiCharacters")
-class SendFriendRequestTest: TestLib() {
+class SendFriendRequestTest: TestFactory() {
+
+    private val testPath = "/users/friends"
 
     @BeforeEach
     fun setup() {
@@ -32,36 +34,45 @@ class SendFriendRequestTest: TestLib() {
     }
 
     @Test
-    fun `친구 요청`() {
+    fun `친구 요청 - Ok`() {
         val request: MultiValueMap<String, String> = LinkedMultiValueMap<String, String>()
             .apply { add("nickname", user2.nickname) }
 
-        requestParams(post("/users/friends"), request, currentUserToken)
+        requestParams(post(testPath), request, currentUserToken)
             .andExpect(MockMvcResultMatchers.status().isOk)
 
-        assert(isConnection(user, user2, FriendStatus.REQUEST))
+        assert(isConnect(user, user2, FriendStatus.REQUEST))
     }
 
     @Test
-    fun `타겟 유저가 존재하지 않음`() {
+    fun `타겟 유저가 존재하지 않음 - NotFound`() {
         val request: MultiValueMap<String, String> = LinkedMultiValueMap<String, String>()
             .apply { add("nickname", "nickname") }
 
-        requestParams(post("/users/friends"), request, currentUserToken)
+        requestParams(post(testPath), request, currentUserToken)
             .andExpect(MockMvcResultMatchers.status().isNotFound)
     }
 
     @Test
-    fun `타겟 유저와 현재 유저와 같은 경우`() {
+    fun `사용자 인증이 확인되지 않음 - Unauthorized`() {
+        val request: MultiValueMap<String, String> = LinkedMultiValueMap<String, String>()
+            .apply { add("nickname", user2.nickname) }
+
+        requestParams(post(testPath), request)
+            .andExpect(MockMvcResultMatchers.status().isUnauthorized)
+    }
+
+    @Test
+    fun `타겟 유저와 현재 유저와 같은 경우 - BadRequest`() {
         val request: MultiValueMap<String, String> = LinkedMultiValueMap<String, String>()
             .apply { add("nickname", user.nickname) }
 
-        requestParams(post("/users/friends"), request, currentUserToken)
+        requestParams(post(testPath), request, currentUserToken)
             .andExpect(MockMvcResultMatchers.status().isBadRequest)
     }
 
     @Test
-    fun `이미 친구 요청이 간 경우`() {
+    fun `이미 친구 요청이 간 경우 - BadRequest`() {
         friendRepository.save(Friend(
             user = user,
             targetUser = user2
@@ -69,12 +80,12 @@ class SendFriendRequestTest: TestLib() {
         val request: MultiValueMap<String, String> = LinkedMultiValueMap<String, String>()
             .apply { add("nickname", user2.nickname) }
 
-        requestParams(post("/users/friends"), request, currentUserToken)
+        requestParams(post(testPath), request, currentUserToken)
             .andExpect(MockMvcResultMatchers.status().isBadRequest)
    }
 
     @Test
-    fun `타겟 유저가 친구 요청을 보냈을 경우`() {
+    fun `타겟 유저가 친구 요청을 보냈을 경우 - BadRequest`() {
         friendRepository.save(Friend(
             user = user2,
             targetUser = user
@@ -82,16 +93,7 @@ class SendFriendRequestTest: TestLib() {
         val request: MultiValueMap<String, String> = LinkedMultiValueMap<String, String>()
             .apply { add("nickname", user2.nickname) }
 
-        requestParams(post("/users/friends"), request, currentUserToken)
+        requestParams(post(testPath), request, currentUserToken)
             .andExpect(MockMvcResultMatchers.status().isBadRequest)
-    }
-
-    @Test
-    fun `사용자 인증이 확인되지 않음`() {
-        val request: MultiValueMap<String, String> = LinkedMultiValueMap<String, String>()
-            .apply { add("nickname", user2.nickname) }
-
-        requestParams(post("/users/friends"), request)
-            .andExpect(MockMvcResultMatchers.status().isUnauthorized)
     }
 }
