f186275f435fcf41fd9924c6e9188a7aceb506f3
kangsinhee <rkdtlsgml40@naver.com>
2022-01-24 13:45:12 +0900

    [MOVED] - jwt Package application.service 패키지로 이동

--- src/main/kotlin/sinhee/kang/tutorial/global/security/jwt/enums/TokenType.kt
+++
@@ -1,6 +0,0 @@
-package sinhee.kang.tutorial.global.security.jwt.enums
-
-enum class TokenType(val tokenName: String, val expiredTokenTime: Long) {
-    ACCESS("access_token", 60L * 60L * 2L),
-    REFRESH("_Refresh", 60L * 60L * 24L * 7L)
-}
